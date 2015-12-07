cdc92c0f01b2b378c90b560b2f5e8d69e4f59289
Nick Owen <nowen@scottlogic.com>
2015-12-07 15:30:38 +0000

    Refactor most recent stocks

--- public/showcase-controller.js
+++ public/showcase-controller.js
@@ -1,9 +1,16 @@
 ﻿(function() {
     'use strict';
 
-    angular.module('openfin', [])
+    angular.module('openfin.showcase', [])
         .controller('ShowcaseCtrl', [function() {
             var self = this;
+
+            self.stocks = [
+                'AAPL',
+                'MSFT',
+                'TITN',
+                'SNDK'
+            ];
 
             self.open = function() {
                 var child = new fin.desktop.Window({
