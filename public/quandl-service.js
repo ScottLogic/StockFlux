09775fb83f76d51b22818d5f6b341502c1812633
Nick Owen <nowen@scottlogic.com>
2015-12-11 11:15:13 +0000

    Fetch Quandl financial data (closes #48)

--- public/quandl-service.js
+++ public/quandl-service.js
@@ -3,8 +3,63 @@
 
     angular.module('openfin.quandl', ['ngResource'])
         .factory('quandlService', ['$resource', function($resource) {
-            return $resource('https://www.quandl.com/api/v3/datasets.json?api_key=kM9Z9aEULVDD7svZ4A8B&query=:query&database_code=WIKI', {}, {
-                get: { method: 'GET', cache: true }
-            });
+            var apiKey = 'api_key=kM9Z9aEULVDD7svZ4A8B';
+
+            function stock() {
+                return $resource('https://www.quandl.com/api/v3/datasets.json?' + apiKey + '&query=:query&database_code=WIKI', {}, {
+                    get: { method: 'GET', cache: true }
+                });
+            }
+
+            function stockData() {
+                var startDate = moment().subtract(1, 'weeks').format('YYYY-MM-DD'),
+                    json;
+
+                return $resource('https://www.quandl.com/api/v3/datasets/WIKI/:code/data.json?' + apiKey + '&start_date=' + startDate, {}, {
+                    get: {
+                        method: 'GET',
+                        transformResponse: function(data, headers) {
+                            json = angular.fromJson(data);
+                            processResponse(json);
+                            return json;
+                        },
+                        cache: true
+                    }
+                });
+            }
+
+            function processResponse(json) {
+                var datasetData = json.dataset_data,
+                    financialData = datasetData.data,
+                    results = [],
+                    i = 0,
+                    max = financialData.length;
+
+                for (i; i < max; i++) {
+                    results.push(extract(financialData[i]));
+                }
+
+                json.stockData = {
+                    startDate: datasetData.start_date,
+                    endDate: datasetData.end_date,
+                    data: results
+                };
+            }
+
+            function extract(data) {
+                return {
+                    date: data[0],
+                    open: data[1],
+                    high: data[2],
+                    low: data[3],
+                    close: data[4],
+                    volume: data[5]
+                };
+            }
+
+            return {
+                stock: stock,
+                stockData: stockData
+            };
         }]);
 }());
--- public/stock-controller.js
+++ public/stock-controller.js
@@ -4,12 +4,39 @@
     angular.module('openfin.stock', ['openfin.quandl'])
        .controller('StockCtrl', ['$routeParams', 'quandlService', function($routeParams, quandlService) {
            var self = this;
+           self.stocks = [];
 
-           self.results = '';
-
-           quandlService.get({ query: $routeParams.query }, function(result) {
+           quandlService.stock().get({ query: $routeParams.query }, function(result) {
                // Re-fetch the cached result.
-               self.results = result.datasets;
+               var fetchedStocks = result.datasets,
+                   length = fetchedStocks.length,
+                   i,
+                   fetchedStock,
+                   stock;
+
+               for (i = 0; i < length; i++) {
+                   fetchedStock = fetchedStocks[i];
+                   self.stocks.push({
+                       name: fetchedStock.name,
+                       code: fetchedStock.dataset_code
+                   });
+               }
+
+               // After the stock meta-data has been fetched and displayed the financial data
+               // can be retrieved and displayed
+               for (i = 0; i < length; i++) {
+                   stock = self.stocks[i];
+                   fetchStockData(stock);
+               }
            });
+
+           function fetchStockData(stock) {
+               quandlService.stockData().get({ code: stock.code }, function(result) {
+                   var data = result.stockData;
+                   stock.data = data.data;
+                   stock.startDate = data.startDate;
+                   stock.endDate = data.endDate;
+               });
+           }
        }]);
 }());
