a0e3918f965a3b9b3c0d03396579959c17120368
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-12-11 09:44:00 +0000

    Added api key for the service. Closes #35

--- public/quandl-service.js
+++ public/quandl-service.js
@@ -3,7 +3,7 @@
 
     angular.module('openfin.quandl', ['ngResource'])
         .factory('quandlService', ['$resource', function($resource) {
-            return $resource('https://www.quandl.com/api/v3/datasets.json?query=:query&database_code=WIKI', {}, {
+            return $resource('https://www.quandl.com/api/v3/datasets.json?api_key=kM9Z9aEULVDD7svZ4A8B&query=:query&database_code=WIKI', {}, {
                 get: { method: 'GET', cache: true }
             });
         }]);
