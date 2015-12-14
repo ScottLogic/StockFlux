c343998eec327594f844aa0f55b527ebcb0c8a0b
Nick Owen <nowen@scottlogic.com>
2015-12-14 15:06:20 +0000

    Save list of searched stocks (part 1)

--- public/thumbnails-controller.js
+++ public/thumbnails-controller.js
@@ -1,20 +1,17 @@
 (function() {
     'use strict';
 
-    angular.module('openfin.thumbnails', [])
-        .controller('ThumbnailsCtrl', [function() {
+    angular.module('openfin.thumbnails', ['openfin.store'])
+        .controller('ThumbnailsCtrl', ['storeService', function(store) {
             var self = this;
+            var visitedNumber = 9;
 
-            self.stocks = [
-                'AAPL',
-                'MSFT',
-                'TITN',
-                'SNDK'
-            ];
+            self.stocks = store.getStocks().slice(0, visitedNumber);
 
-            self.open = function(stock) {
+            self.open = function(stockName) {
+                store.incrementStock(stockName);
                 var child = new fin.desktop.Window({
-                    name: stock + ' Stock Data',
+                    name: stockName + ' Stock Data',
                     url: 'd3fc-showcase.html'
                 }, function() {
                     child.show();
