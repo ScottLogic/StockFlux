38ef8f913acf79ba44229176085459fcf9e6ca48
Nick Owen <nowen@scottlogic.com>
2016-01-12 14:04:04 +0000

    Move minichart rendering into separate controller/directive

---
+++ src/sidebars/favourites/minichart/minichart-directive.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('openfin.minichart')
+        .directive('minichart', [function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'sidebars/favourites/minichart/minichart.html',
+                scope: {
+                    renderChart: '&',
+                    stock: '='
+                }
+            };
+        }])
+}());
