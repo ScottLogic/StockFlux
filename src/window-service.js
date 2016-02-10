2a300b6c29e528abe8383d435611bb5dfb22ee22
Nick Owen <nowen@scottlogic.com>
2016-02-10 12:10:00 +0000

    Update WindowService

--- src/window-service.js
+++ src/window-service.js
@@ -1,108 +1,101 @@
 (function(fin) {
     'use strict';
+    var firstName = true;
+
+    function getName() {
+        if (firstName) {
+            firstName = false;
+            return 'main';
+        }
+
+        // TODO: Should probably change this...
+        return 'window' + Math.floor(Math.random() * 1000) + Math.ceil(Math.random() * 999);
+    }
+
+    class AppManager {
+        constructor() {
+            this.windowsOpen = 0;
+        }
+
+        increment() {
+            this.windowsOpen++;
+        }
+
+        decrement() {
+            this.windowsOpen--;
+
+            if (this.windowsOpen === 0) {
+                window.close();
+            }
+        }
+    }
+
+    class WindowCreationService {
+        constructor(storeService) {
+            this.storeService = storeService;
+            this.openWindows = {};
+            this.windowsCache = [];
+            this.firstName = true;
+            this.apps = new AppManager();
+        }
+
+        createWindow(config, successCb) {
+            config.name = getName();
+            var newWindow = new fin.desktop.Window(config, () => {
+                this.windowsCache.push(newWindow);
+
+                // TODO
+                // Begin super hack
+                newWindow.getNativeWindow().windowService = this;
+                newWindow.getNativeWindow().storeService = this.storeService;
+                // End super hack
+
+                if (successCb) {
+                    successCb(newWindow);
+                }
+            });
+
+            this.apps.increment();
+
+            newWindow.addEventListener('closed', (e) => {
+                var parent = this.openWindows[newWindow.name];
+                if (parent) {
+                    for (var i = 0, max = parent.length; i < max; i++) {
+                        parent[i].close();
+                    }
+                }
+
+                var index = this.windowsCache.indexOf(newWindow);
+                this.windowsCache.slice(index, 1);
+
+                this.apps.decrement();
+            });
+
+            return newWindow;
+        }
+
+        createTearoutWindow(config, parentName) {
+            var tearoutWindow = this.createWindow(config);
+
+            if (!this.openWindows[parentName]) {
+                this.openWindows[parentName] = [].concat(tearoutWindow);
+            } else {
+                this.openWindows[parentName].push(tearoutWindow);
+            }
+
+            return tearoutWindow;
+        }
+
+        ready(cb) {
+            fin.desktop.main(cb);
+        }
+
+        getWindows() {
+            return this.windowsCache;
+        }
+    }
+    WindowCreationService.$inject = ['storeService'];
 
     angular.module('openfin.window')
-        .factory('windowCreationService', ['storeService', function(storeService) {
-            var self = this;
-            var openWindows = {},
-                windowsCache = [],
-                firstName = true;
-
-            function appManager() {
-                var windowsOpen = 0;
-
-                function increment() {
-                    windowsOpen++;
-                }
-
-                function decrement() {
-                    windowsOpen--;
-
-                    if (windowsOpen === 0) {
-                        window.close();
-                    }
-                }
-
-                return {
-                    increment: increment,
-                    decrement: decrement
-                };
-            }
-
-            var apps = appManager();
-
-            function getName() {
-                if (firstName) {
-                    firstName = false;
-                    return 'main';
-                }
-
-                // TODO: Should probably change this...
-                return 'window' + Math.floor(Math.random() * 1000) + Math.ceil(Math.random() * 999);
-            }
-
-            self.createWindow = function(config, successCb) {
-                config.name = getName();
-                var newWindow = new fin.desktop.Window(config, function() {
-                    windowsCache.push(newWindow);
-
-                    // TODO
-                    // Begin super hack
-                    newWindow.getNativeWindow().windowService = self;
-                    newWindow.getNativeWindow().storeService = storeService;
-                    // End super hack
-
-                    if (successCb) {
-                        successCb(newWindow);
-                    }
-                });
-
-                apps.increment();
-
-                newWindow.addEventListener('closed', function(e) {
-                    var parent = openWindows[newWindow.name];
-                    if (parent) {
-                        for (var i = 0, max = parent.length; i < max; i++) {
-                            parent[i].close();
-                        }
-                    }
-
-                    apps.decrement();
-
-                    var index = windowsCache.indexOf(newWindow);
-                    windowsCache.slice(index, 1);
-
-                    // TODO: Need to set window's closed state to true in the store.
-                });
-
-                return newWindow;
-            };
-
-            self.createTearoutWindow = function(config, parentName) {
-                var tearoutWindow = self.createWindow(config);
-
-                if (!openWindows[parentName]) {
-                    openWindows[parentName] = [].concat(tearoutWindow);
-                } else {
-                    openWindows[parentName].push(tearoutWindow);
-                }
-
-                return tearoutWindow;
-            };
-
-            function ready(cb) {
-                fin.desktop.main(cb);
-            }
-
-            function getWindows() {
-                return windowsCache;
-            }
-
-            return {
-                createWindow: self.createWindow,
-                createTearoutWindow: self.createTearoutWindow,
-                ready: ready,
-                getWindows: getWindows
-            };
-        }]);
+        .service('windowCreationService', WindowCreationService);
 }(fin));
