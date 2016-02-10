5dfd8693424e91a3c1a12f0ac70ee494ecf267ab
Adam Morgan <akhmorgan@hotmail.com>
2016-02-10 11:27:39 +0000

    resizes window to compact size based on summary icon click

--- src/toolbar/toolbar-controller.js
+++ src/toolbar/toolbar-controller.js
@@ -6,6 +6,7 @@
             this.$timeout = $timeout;
             this.currentWindowService = currentWindowService;
             this.maximised = false;
+            this.summarised = false;
             currentWindowService.ready(this.onReady.bind(this));
         }
 
@@ -34,6 +35,20 @@
 
         normalSizeClick() {
             this.window.restore();
+        }
+
+        summariseClick() {
+            this.summarised = !this.summarised;
+            this.window.summarised = this.summarised;
+            if (this.summarised) {
+                this.window.resizeTo(280, 500, 'top-right');
+            }
+            else if (this.maximised) {
+                this.window.maximize();
+            }
+            else {
+                this.window.resizeTo(1280, 720, 'top-right');
+            }
         }
 
         closeClick() {
