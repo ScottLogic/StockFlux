eb924e32f01d336dc8ac4ca992ecaf1b2d6c9b99
Nick Owen <nowen@scottlogic.com>
2016-02-25 15:07:49 +0000

    Don't show closed window if it's empty

--- src/closedWindows/closedWindows-controller.js
+++ src/closedWindows/closedWindows-controller.js
@@ -2,8 +2,18 @@
     'use strict';
 
     class ClosedWindowsCtrl {
+        constructor() {
+            this.closedWindows = [];
+            this.closedTabsShow = false;
+        }
+
         refreshClosedWindows() {
             this.closedWindows = window.storeService.getPreviousClosedWindows();
+        }
+
+        click() {
+            this.refreshClosedWindows();
+            this.closedTabsShow = this.closedWindows.length > 0;
         }
     }
     ClosedWindowsCtrl.$inject = [];
