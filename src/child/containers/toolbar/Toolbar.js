c68622db6f739d6fd0177368bf5c00a923503565
Martinho Santos <lamppostman@gmail.com>
2016-04-26 14:36:44 +0100

    Redux toolbar refactor (#781)
    * Small toolbar refactor

--- src/child/containers/toolbar/Toolbar.js
+++ src/child/containers/toolbar/Toolbar.js
@@ -5,8 +5,7 @@ import {
     maximise,
     unMaximise,
     compact,
-    expand,
-    close
+    expand
 } from '../../actions/window.js';
 import icon from '../../assets/png/scottlogic_logo.png';
 
@@ -60,21 +59,18 @@ class Toolbar extends Component {
     render() {
         const { windowState } = this.props;
         return (
-            <div className="toolbarWrapper">
-                <div className="drag">&nbsp;</div>
-                <div className="wrapper left">
-                    <img id="logo" src={icon} alt="Scott Logic" />
-                </div>
+            <div className="toolbarWrapper drag">
+                <img id="logo" src={icon} className="logo" alt="Scott Logic" />
                 <div className="wrapper action">
                     <div className="icon minimise" onClick={this.onMinimize} title="Minimize">&nbsp;</div>
                     {windowState.isCompact
                         ? <div className="icon full_view" onClick={this.onFullView} title="Full View">&nbsp;</div>
                         : <div className="icon compact" onClick={this.onCompact} title="Compact View">&nbsp;</div>
                     }
-                    {windowState.isMaximised
+                    {!windowState.isCompact && (windowState.isMaximised
                         ? <div className="icon normal_size" onClick={this.onUnMaximise} title="Restore" ng-show="toolbarCtrl.maximised && !toolbarCtrl.isCompact()">&nbsp;</div>
                         : <div className="icon maximise" onClick={this.onMaximise} title="Maximise" ng-show="!toolbarCtrl.maximised && !toolbarCtrl.isCompact()">&nbsp;</div>
-                    }
+                    )}
                     <div className="icon close" onClick={this.onClose} title="Close">&nbsp;</div>
                 </div>
             </div>
