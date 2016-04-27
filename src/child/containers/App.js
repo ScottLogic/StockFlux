4945c17f31377fb1ca99c065a1faf51f4a8a7c79
Mike Smith <mic-smith@users.noreply.github.com>
2016-04-27 15:36:28 +0100

    Initial charts and updates to styles to prevent conflicts (#785)
    Will fix travis build as a follow

--- src/child/containers/App.js
+++ src/child/containers/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Toolbar from './toolbar/Toolbar.js';
 import SideBar from './sidebars/Sidebar.js';
+import Showcase from './showcase/Showcase';
+import { truncate } from '../services/formatters';
+
 
 import DevTools from './devTools/DevTools.js';
+import '../../../node_modules/d3fc/dist/d3fc.min.css';
+import '../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
+require('script!../../../node_modules/jquery/dist/jquery.min.js');
+require('script!../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
+require('script!../../../node_modules/moment/moment.js');
+require('script!../../../node_modules/BitFlux/node_modules/bootstrap/js/dropdown.js');
+require('script!../../../node_modules/d3fc/dist/d3fc.bundle.min.js');
+require('script!../../../node_modules/BitFlux/dist/bitflux.js');
 
-const App = () => (
+const App = ({ code, name }) => (
     <div className="main">
         <SideBar />
         <div className="main-content">
             <Toolbar />
+            <div id="showcase-title">
+                <div className="code">{code}</div> <div className="name"> {truncate(name)}</div>
+            </div>
+            <Showcase code={code} />
         </div>
         <DevTools />
     </div>
 );
 
-export default connect()(App);
+App.propTypes = {
+    code: PropTypes.string,
+    name: PropTypes.string
+};
+
+function mapStateToProps(state) {
+    const { selection } = state;
+    const { name, code } = selection;
+    return { name, code };
+}
+
+export default connect(mapStateToProps)(App);
