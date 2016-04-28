a6de08b51ecb5fc828785fbbb519ba22aaccca66
Mike Smith <mic-smith@users.noreply.github.com>
2016-04-28 14:54:50 +0100

    Add version link and minor changes (#789)

--- src/child/components/SearchResult.js
+++ src/child/components/SearchResult.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react';
+import { truncate } from '../services/formatters';
 
 const Stock = ({ isFavourite, stock, bindings }) => (
     <div className="search-result">
         <div className="details" onClick={() => bindings.onClick(stock)}>
-            <div className="name">{stock.name}</div>
+            <div className="name">{truncate(stock.name)}</div>
             <div className="code">{stock.code}</div>
         </div>
         <div className={'button-icon star' + (isFavourite ? ' active' : '')} onClick={() => bindings.onIconClick(stock)}>
