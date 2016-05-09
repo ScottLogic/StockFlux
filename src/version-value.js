09b9cdd2c57a32e18119fbdfadf241e943987565
Mike Smith <mic-smith@users.noreply.github.com>
2016-05-09 10:38:18 +0100

    Release 10.2.0

--- src/version-value.js
+++ src/version-value.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    const VERSION = { version: '10.1.0' };
+    const VERSION = { version: '10.2.0' };
 
     angular.module('stockflux.version')
         .value('Version', VERSION.version);
