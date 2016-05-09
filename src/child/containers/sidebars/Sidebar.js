2d87955bb09f0a7a1c6aaa489bff6c7c84c04b33
Mike Smith <mic-smith@users.noreply.github.com>
2016-05-09 14:08:45 +0100

    fix: tidy up sidebar code and remove unused callback in quandl

--- src/child/containers/sidebars/Sidebar.js
+++ src/child/containers/sidebars/Sidebar.js
@@ -1,16 +1,12 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
-import Favourites from './Favourites/Favourites.js';
-import Search from './Search/Search.js';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Favourites from './favourites/Favourites.js';
+import Search from './search/Search.js';
 
-import {selectFavourites, selectSearch} from '../../actions/sidebar';
+import { selectFavourites, selectSearch } from '../../actions/sidebar';
 
-function notifyParent(eventName, payload) {
-    var e = new CustomEvent(eventName, {'detail': payload});
-    window.opener.dispatchEvent(e);
-}
 
-export default class Sidebar extends Component {
+class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.focusFav = this.focusFav.bind(this);
@@ -18,30 +14,33 @@ export default class Sidebar extends Component {
     }
 
     focusFav() {
-        !this.props.sidebar.showFavourites && this.props.dispatch(selectFavourites());
+        if (!this.props.sidebar.showFavourites) {
+            this.props.dispatch(selectFavourites());
+        }
     }
 
     focusSearch() {
-        !this.props.sidebar.showSeach && this.props.dispatch(selectSearch());
+        if (!this.props.sidebar.showSearch) {
+            this.props.dispatch(selectSearch());
+        }
     }
 
     render() {
-        const {sidebar} = this.props;
+        const { sidebar } = this.props;
 
         return (
             <div className="sidebars">
-                <div className={'search main-search ' + (sidebar.showSeach ? 'expanded' : 'contracted')} onClick={this.focusSearch}>
-                    <Search/>
+                <div className={`search main-search ${sidebar.showSearch ? 'expanded' : 'contracted'}`} onClick={this.focusSearch}>
+                    <Search />
                 </div>
-                <div className={'favourites ' + (sidebar.showFavourites ? 'expanded' : 'contracted')} onClick={this.focusFav}>
-                    <Favourites/>
+                <div className={`favourites ${sidebar.showFavourites ? 'expanded' : 'contracted'}`} onClick={this.focusFav}>
+                    <Favourites />
                 </div>
 
                 <div className="closed-window-selection">
                     <closed-window-list icon="'closed_tabs'"></closed-window-list>
                 </div>
             </div>
-
         );
     }
 }
@@ -51,7 +50,7 @@ Sidebar.propTypes = {
 };
 
 function mapStateToProps(state) {
-    var {sidebar} = state;
-    return {sidebar};
+    const { sidebar } = state;
+    return { sidebar };
 }
 export default connect(mapStateToProps)(Sidebar);
