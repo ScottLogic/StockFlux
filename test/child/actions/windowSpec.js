e54ecf2bf76dbdb370ca20103c69622825dbd646
James Leftley <jleft@users.noreply.github.com>
2016-05-16 16:04:04 +0100

    test: add tests for window actions (#808)

---
+++ test/child/actions/windowSpec.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import * as actions from '../../../src/child/actions/window';
+import { WINDOW as ACTION_TYPES } from '../../../src/child/constants/actionTypes.js';
+
+describe('window actions', () => {
+    it('should create an action for minimize', () => {
+        const expectedAction = { type: ACTION_TYPES.MINIMIZE };
+        expect(actions.minimise()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for compact', () => {
+        const expectedAction = {
+            type: ACTION_TYPES.TOGGLE_COMPACT,
+            state: true
+        };
+        expect(actions.compact()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for expand', () => {
+        const expectedAction = {
+            type: ACTION_TYPES.TOGGLE_COMPACT,
+            state: false
+        };
+        expect(actions.expand()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for full view', () => {
+        const expectedAction = { type: ACTION_TYPES.STATE_FULL_VIEW };
+        expect(actions.fullView()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for maximize', () => {
+        const expectedAction = { type: ACTION_TYPES.MAXIMIZE };
+        expect(actions.maximize()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for restore', () => {
+        const expectedAction = { type: ACTION_TYPES.RESTORE };
+        expect(actions.restore()).to.deep.equal(expectedAction);
+    });
+
+    it('should create an action for close', () => {
+        const expectedAction = { type: ACTION_TYPES.CLOSE };
+        expect(actions.close()).to.deep.equal(expectedAction);
+    });
+});
