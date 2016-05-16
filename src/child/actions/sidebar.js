070dbeebc178190d0a529fe477a589953ace0ada
Robert James McNeill <rjmcneill@users.noreply.github.com>
2016-05-16 11:16:44 +0100

    infrastructure: Moved actions to enum file (#803)

--- src/child/actions/sidebar.js
+++ src/child/actions/sidebar.js
@@ -1,80 +1,72 @@
-export const SEARCH_CLICKED = 'SEARCH_CLICKED';
-export const SEARCH_STARTED = 'SEARCH_STARTED';
-export const SEARCH_INPUT = 'SEARCH_INPUT';
-export const SEARCH_ERROR = 'SEARCH_ERROR';
-export const CLEAR_SEARCH = 'CLEAR_SEARCH';
-export const SEARCH_FINISHED = 'SEARCH_FINISHED';
-export const FAV_CLICKED = 'FAV_CLICKED';
-export const TOGGLE_FAVOURITE = 'TOGGLE_FAVOURITE';
-export const SELECTION = 'SELECTION';
-export const UNSELECT = 'UNSELECT';
+import { SIDEBAR as ACTION_TYPES } from '../constants/actionTypes.js';
 
 import QuandlService from '../services/QuandlService.js';
 const quandlService = new QuandlService();
 
 export function searchInput(term) {
     return {
-        type: SEARCH_INPUT,
+        type: ACTION_TYPES.SEARCH_INPUT,
         term
     };
 }
 
 export function selectStock(code, name) {
     return {
+        type: ACTION_TYPES.SELECTION,
         code,
-        name,
-        type: SELECTION
+        name
     };
 }
 
 export function unselectStock() {
     return {
-        type: UNSELECT
+        type: ACTION_TYPES.UNSELECT
     };
 }
 
 export function toggleFavourite(code) {
     return {
-        code,
-        type: TOGGLE_FAVOURITE
+        type: ACTION_TYPES.TOGGLE_FAVOURITE,
+        code
     };
 }
 
 export function clearSearch() {
     return {
-        type: CLEAR_SEARCH
+        type: ACTION_TYPES.CLEAR_SEARCH
     };
 }
 
 function searchStarted(term) {
     return {
-        type: SEARCH_STARTED,
+        type: ACTION_TYPES.SEARCH_STARTED,
         term
     };
 }
 
 export function searchFinished(term, results) {
     return {
-        type: SEARCH_FINISHED,
+        type: ACTION_TYPES.SEARCH_FINISHED,
         term,
         results
     };
 }
+
 export function searchError() {
     return {
-        type: SEARCH_ERROR
+        type: ACTION_TYPES.SEARCH_ERROR
     };
 }
 
 export function selectSearch() {
     return {
-        type: SEARCH_CLICKED
+        type: ACTION_TYPES.SEARCH_CLICKED
     };
 }
 
 export function selectFavourites() {
     return {
-        type: FAV_CLICKED
+        type: ACTION_TYPES.FAV_CLICKED
     };
 }
 
