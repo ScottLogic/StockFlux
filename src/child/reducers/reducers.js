5107e6d00a48d62e6356c2ff0ade1df67977057a
James Leftley <jleftley@scottlogic.com>
2016-05-25 08:25:06 +0100

    refactor: separate reducers into separate files

--- src/child/reducers/reducers.js
+++ src/child/reducers/reducers.js
@@ -1,133 +1,11 @@
 import { combineReducers } from 'redux';
-
-import ACTION_TYPES from '../constants/actionTypes.js';
 import deepFreeze from 'deep-freeze';
 
-function selection(state = {}, action) {
-    switch (action.type) {
-    case ACTION_TYPES.SIDEBAR.SELECTION:
-        return Object.assign({}, state, {
-            code: action.code,
-            name: action.name,
-        });
-    case ACTION_TYPES.SIDEBAR.UNSELECT:
-        return {};
-    default:
-        return state;
-    }
-}
-
-function sidebar(state = { showSearch: true }, action) {
-    switch (action.type) {
-    case ACTION_TYPES.SIDEBAR.FAV_CLICKED:
-        return {
-            showFavourites: true
-        };
-    case ACTION_TYPES.SIDEBAR.SEARCH_CLICKED:
-        return {
-            showSearch: true
-        };
-    default:
-        return state;
-    }
-}
-
-function search(state = { term: '' }, action) {
-    switch (action.type) {
-    case ACTION_TYPES.SIDEBAR.SEARCH_INPUT:
-        return Object.assign({}, state, {
-            term: action.term
-        });
-    case ACTION_TYPES.SIDEBAR.SEARCH_STARTED:
-        return Object.assign({}, state, {
-            isSearching: true,
-            term: action.term
-        });
-    case ACTION_TYPES.SIDEBAR.SEARCH_FINISHED:
-        return state.term === action.term ? Object.assign({}, state, {
-            isSearching: false,
-            results: action.results
-        }) : state;
-    case ACTION_TYPES.SIDEBAR.SEARCH_ERROR:
-        return Object.assign({}, state, {
-            isSearching: false,
-            hasErrors: true
-        });
-    case ACTION_TYPES.SIDEBAR.CLEAR_SEARCH:
-        return { term: '' };
-    default:
-        return state;
-    }
-}
-
-
-function favourites(state = { codes: [], names: {} }, action) {
-    let newState;
-    let codes;
-    let index;
-    let currentIndex;
-
-    switch (action.type) {
-    case ACTION_TYPES.SIDEBAR.TOGGLE_FAVOURITE:
-        index = state.codes.indexOf(action.code);
-        newState = Object.assign({}, state, {
-            codes: [...state.codes],
-            names: Object.assign({}, state.names)
-        });
-        if (index >= 0) {
-            newState.codes.splice(index, 1);
-            delete newState.names[action.code];
-        } else {
-            newState.codes.push(action.code);
-        }
-        return newState;
-    case ACTION_TYPES.SIDEBAR.INSERT_FAVOURITE_AT:
-        codes = [...state.codes];
-        currentIndex = codes.indexOf(action.code);
-        if (currentIndex >= 0) {
-            codes.splice(currentIndex, 1);
-        }
-        index = action.index;
-        if (index >= 0) {
-            codes.splice(index, 0, action.code);
-        } else {
-            codes.push(action.code);
-        }
-        return Object.assign({}, state, { codes, move: {} });
-    case ACTION_TYPES.SIDEBAR.QUANDL_RESPONSE:
-        newState = Object.assign({}, state, {
-            names: Object.assign({}, state.names, {
-                [action.code]: action.name
-            })
-        });
-        return newState;
-    default:
-        return state;
-    }
-}
-
-function windowState(state = {
-    isCompact: false,
-    isMaximised: false
-}, action) {
-    switch (action.type) {
-    case ACTION_TYPES.WINDOW.TOGGLE_COMPACT:
-        return Object.assign({}, state, {
-            isCompact: action.state
-        });
-    case ACTION_TYPES.WINDOW.MAXIMIZE:
-        return Object.assign({}, state, {
-            isMaximised: true
-        });
-    case ACTION_TYPES.WINDOW.RESTORE:
-        return Object.assign({}, state, {
-            isMaximised: false
-        });
-    case ACTION_TYPES.WINDOW.CLOSE:
-    default:
-        return state;
-    }
-}
+import selection from './selection';
+import sidebar from './sidebar';
+import favourites from './favourites';
+import search from './search';
+import windowState from './windowState';
 
 const rootReducer = combineReducers({
     selection,
@@ -139,13 +17,13 @@ const rootReducer = combineReducers({
 
 // Wrap the reducer in dev to freeze the state and action
 const checkImmutable = (reducer) => {
-
     if (process.env.NODE_ENV === 'production') {
         return reducer;
     }
-    return (state, action) => reducer(state !== undefined ? deepFreeze(state) : state,
-                                        action !== undefined ? deepFreeze(action) : action);
+    return (state, action) => reducer(
+        state !== undefined ? deepFreeze(state) : state,
+        action !== undefined ? deepFreeze(action) : action
+    );
 };
-
 
 export default checkImmutable(rootReducer);
---
+++ src/child/reducers/selection.js
@@ -0,0 +1,15 @@
+import { SIDEBAR as ACTION_TYPES } from '../constants/actionTypes';
+
+export default function selection(state = {}, action) {
+    switch (action.type) {
+    case ACTION_TYPES.SELECTION:
+        return Object.assign({}, state, {
+            code: action.code,
+            name: action.name,
+        });
+    case ACTION_TYPES.UNSELECT:
+        return {};
+    default:
+        return state;
+    }
+}
