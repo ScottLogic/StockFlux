0dc06898365d7f8aa7a5db2626cb40bcc8fdb93b
James Leftley <jleft@users.noreply.github.com>
2016-05-30 21:11:02 +0100

    refactor: separate reducers into separate files (#820)

---
+++ src/child/reducers/windowState.js
@@ -0,0 +1,24 @@
+import { WINDOW as ACTION_TYPES } from '../constants/actionTypes';
+
+export default function windowState(state = {
+    isCompact: false,
+    isMaximised: false
+}, action) {
+    switch (action.type) {
+    case ACTION_TYPES.TOGGLE_COMPACT:
+        return Object.assign({}, state, {
+            isCompact: action.state
+        });
+    case ACTION_TYPES.MAXIMIZE:
+        return Object.assign({}, state, {
+            isMaximised: true
+        });
+    case ACTION_TYPES.RESTORE:
+        return Object.assign({}, state, {
+            isMaximised: false
+        });
+    case ACTION_TYPES.CLOSE:
+    default:
+        return state;
+    }
+}
