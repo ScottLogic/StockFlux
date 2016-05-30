b6619619615af64ad322841bcf516d4afbb1f368
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-05-30 14:18:36 +0100

    feat: Completed multichild support and spawning for parent store

--- src/parent/parentStore.js
+++ src/parent/parentStore.js
@@ -7,12 +7,14 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import reducer from './reducer.js';
 import DevTools from '../child/containers/devTools/DevTools.js';
+import persistState from 'redux-localstorage';
 
 const enhancer = compose(
     // Middleware you want to use in development:
     applyMiddleware(thunkMiddleware, createLogger()),
     // Required! Enable Redux DevTools with the monitors you chose
-    DevTools.instrument()
+    DevTools.instrument(),
+    persistState()
 );
 
 export default function parentStore(initialState) {
