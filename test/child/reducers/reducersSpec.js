5245b380eec8ca7cd018c2367d46eb778671eac2
James Leftley <jleftley@scottlogic.com>
2016-05-31 08:37:09 +0100

    test: add initial test for root reducer

---
+++ test/child/reducers/reducersSpec.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import reducers from '../../../src/child/reducers/reducers';
+
+describe('child/reducers/reducers', () => {
+    it('should return the initial state', () => {
+        const initialState = {
+            favourites: {
+                codes: [],
+                names: {}
+            },
+            search: {
+                term: ''
+            },
+            selection: {},
+            sidebar: {
+                showSearch: true
+            },
+            windowState: {
+                isCompact: false,
+                isMaximised: false
+            },
+        };
+        expect(reducers(undefined, {})).to.deep.equal(initialState);
+    });
+});
