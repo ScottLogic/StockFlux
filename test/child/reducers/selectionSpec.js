ae749d9826ca1517ef718293d1a404bed559c451
James Leftley <jleft@users.noreply.github.com>
2016-05-31 19:58:52 +0100

    test: add tests for selection reducer (#827)

---
+++ test/child/reducers/selectionSpec.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import selection from '../../../src/child/reducers/selection';
+import { SIDEBAR as ACTION_TYPES } from '../../../src/child/constants/actionTypes';
+
+describe('child/reducers/selection', () => {
+    it('should return the initial state', () => {
+        expect(selection(undefined, {})).to.deep.equal({});
+    });
+
+    it('should handle SELECTION', () => {
+        const action = {
+            type: ACTION_TYPES.SELECTION,
+            code: 'stockcode',
+            name: 'Stock Name'
+        };
+        expect(selection({}, action)).to.deep.equal({
+            code: 'stockcode',
+            name: 'Stock Name'
+        });
+        expect(selection({
+            code: 'GOOG',
+            name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+        }, action)).to.deep.equal({
+            code: 'stockcode',
+            name: 'Stock Name'
+        });
+    });
+
+    it('should handle UNSELECT', () => {
+        const action = { type: ACTION_TYPES.UNSELECT };
+        expect(selection({}, action)).to.deep.equal({});
+        expect(selection({
+            code: 'GOOG',
+            name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+        }, action)).to.deep.equal({});
+    });
+
+    it('should return the previous state for any unknown action', () => {
+        const action = { type: 'UNKNOWN_ACTION' };
+        const code = 'GOOG';
+        const name = 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume';
+        expect(selection({}, action)).to.deep.equal({});
+        expect(selection({
+            code,
+            name
+        }, action)).to.deep.equal({
+            code,
+            name
+        });
+    });
+});
