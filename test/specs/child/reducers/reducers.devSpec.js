73cdfb3c4a26a41412a707113dc3058b5a4a7504
James Leftley <jleftley@scottlogic.com>
2016-06-09 17:18:37 +0100

    test: move reducers.dev and reducers.prod specs to correct directory

--- test/specs/child/reducers/reducers.devSpec.js
+++ test/specs/child/reducers/reducers.devSpec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import reducers from '../../../src/child/reducers/reducers.dev.js';
+import reducers from '../../../../src/child/reducers/reducers.dev.js';
 
 describe('child/reducers/reducers.dev', () => {
     it('should return the reducer', () => {
