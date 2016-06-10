caaee2a66b33b33ed8a68f59dc2aa62882495b32
James Leftley <jleftley@scottlogic.com>
2016-06-10 14:09:56 +0100

    test: add tests for sidebar search async action creator

--- src/child/actions/sidebar.js
+++ src/child/actions/sidebar.js
@@ -38,33 +38,6 @@ export function toggleFavourite(code) {
     };
 }
 
-export function clearSearch() {
-    return {
-        type: ACTION_TYPES.CLEAR_SEARCH
-    };
-}
-
-function searchStarted(term) {
-    return {
-        type: ACTION_TYPES.SEARCH_STARTED,
-        term
-    };
-}
-
-export function searchFinished(term, results) {
-    return {
-        type: ACTION_TYPES.SEARCH_FINISHED,
-        term,
-        results
-    };
-}
-
-export function searchError() {
-    return {
-        type: ACTION_TYPES.SEARCH_ERROR
-    };
-}
-
 export function selectSearch() {
     return {
         type: ACTION_TYPES.SEARCH_CLICKED
@@ -85,15 +58,41 @@ export function quandlResponse(code, name) {
     };
 }
 
+function clearSearch() {
+    return {
+        type: ACTION_TYPES.CLEAR_SEARCH
+    };
+}
+
+function searchStarted(term) {
+    return {
+        type: ACTION_TYPES.SEARCH_STARTED,
+        term
+    };
+}
+
+function searchFinished(term, results) {
+    return {
+        type: ACTION_TYPES.SEARCH_FINISHED,
+        term,
+        results
+    };
+}
+
+function searchError() {
+    return {
+        type: ACTION_TYPES.SEARCH_ERROR
+    };
+}
+
 export function search(term) {
     return dispatch => {
         if (term.trim() === '') {
-            dispatch(clearSearch());
-        } else {
-            dispatch(searchStarted(term));
-            quandlServiceSearch(term)
-                .then(results => dispatch(searchFinished(term, results)))
-                .catch(() => dispatch(searchError()));
+            return Promise.resolve(dispatch(clearSearch()));
         }
+        dispatch(searchStarted(term));
+        return quandlServiceSearch(term)
+            .then(results => dispatch(searchFinished(term, results)))
+            .catch(() => dispatch(searchError()));
     };
 }
