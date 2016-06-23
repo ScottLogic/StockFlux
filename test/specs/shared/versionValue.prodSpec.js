6cc0cf6ea4fb79e6de8726448f463ec7a17b1b76
James Leftley <jleftley@scottlogic.com>
2016-06-23 17:06:58 +0100

    feat: add separate development and production version values

--- test/specs/shared/versionValue.prodSpec.js
+++ test/specs/shared/versionValue.prodSpec.js
@@ -1,10 +1,10 @@
 import { expect } from 'chai';
 import semver from 'semver';
-import versionValue from '../../../src/shared/versionValue';
+import versionValue from '../../../src/shared/versionValue.prod';
 
-describe('shared/versionValue', () => {
+describe('shared/versionValue.prod', () => {
     it('should be a valid semantic version', () => {
         expect(semver.valid(versionValue)).to.equal(versionValue);
-        expect(semver.valid(versionValue)).to.not.be.null;
+        expect(semver.valid(versionValue)).not.to.be.null;
     });
 });
