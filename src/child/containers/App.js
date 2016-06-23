5c9c1e6283fd2a7438cc122a831794bd2129b653
James Leftley <jleftley@scottlogic.com>
2016-06-23 14:01:12 +0100

    refactor: move styles and global script imports to child index

--- src/child/containers/App.js
+++ src/child/containers/App.js
@@ -7,15 +7,7 @@ import Version from '../components/version/Version';
 import { truncate } from '../services/formatters';
 
 import DevTools from './devTools/DevTools.js';
-import '../../../node_modules/d3fc/dist/d3fc.min.css';
-import '../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
 import noFavourites from '../assets/png/no_favourites.png';
-require('script!../../../node_modules/jquery/dist/jquery.min.js');
-require('script!../../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
-require('script!../../../node_modules/moment/moment.js');
-require('script!../../../node_modules/BitFlux/node_modules/bootstrap/js/dropdown.js');
-require('script!../../../node_modules/d3fc/dist/d3fc.bundle.min.js');
-require('script!../../../node_modules/BitFlux/dist/bitflux.js');
 
 const App = ({ code, name }) => (
     <div className="main">
