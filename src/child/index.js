5705255163bbe77531309208490fdb50ed6f3d2c
James Leftley <jleft@users.noreply.github.com>
2016-07-01 11:54:11 +0100

    refactor: move styles and global script imports to child index (#850)

--- src/child/index.js
+++ src/child/index.js
@@ -6,6 +6,17 @@ import 'babel-polyfill';
 import configureStore from './store/configureStore';
 
 import './assets/styles/style.less';
+import '../../node_modules/d3fc/dist/d3fc.min.css';
+import '../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
+
+/* eslint-disable import/no-unresolved */
+require('script!../../node_modules/jquery/dist/jquery.min.js');
+require('script!../../node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js');
+require('script!../../node_modules/moment/moment.js');
+require('script!../../node_modules/BitFlux/node_modules/bootstrap/js/dropdown.js');
+require('script!../../node_modules/d3fc/dist/d3fc.bundle.min.js');
+require('script!../../node_modules/BitFlux/dist/bitflux.js');
+/* eslint-enable import/no-unresolved */
 
 const store = configureStore();
 
