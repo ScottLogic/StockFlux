a9800275eeb3338a788a0ac926f325a037775d63
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-07-18 15:05:02 +0100

    chore: Added deployable build from travis

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ set -eo pipefail
 # Check for release branch - not using grep as set -e means it fails script
 RELEASE_BRANCH=$(echo "$TRAVIS_BRANCH" |  sed -n 's/^release\-/&/p')
 
-#Get the release type (dev/master) from the branch name
+# Get the release type (dev/master) from the branch name
 TYPE="$TRAVIS_BRANCH"
 
-if ([ "$TRAVIS_PULL_REQUEST" == "false" ]  && [ "${TRAVIS_REPO_SLUG}" == "ScottLogic/StockFlux" ] && ([ "$TYPE" == "dev" ] || [ "$TYPE" == "master" ] || [ -n "$RELEASE_BRANCH" ]))
+if ([ "$TRAVIS_PULL_REQUEST" == "false" ]  && [ "${TRAVIS_REPO_SLUG}" == "rjmcneill/StockFlux" ] && ([ "$TYPE" == "chore/deployable-build" ] || [ "$TYPE" == "dev" ] || [ "$TYPE" == "master" ] || [ -n "$RELEASE_BRANCH" ]))
 then
-    #Clone the latest gh-pages
-    git clone https://github.com/ScottLogic/StockFlux.git --branch gh-pages gh-pages
+    # Clone the latest gh-pages
+    git clone https://github.com/rjmcneill/StockFlux.git --branch gh-pages gh-pages
 
-    #Get line with version from the file -> get the second word -> remove quotes around the value
-    VERSION=$(grep "version" package.json | awk -v N=$2 '{print $2}' | cut -d \" -f2)
+    # Get line with version from the file -> get the second word -> remove quotes around the value
+    VERSION=$(grep -m 1 "version" package.json | awk -v N=$2 '{print $2}' | cut -d \" -f2)
 
     echo "Type is: $TYPE"
     echo "Version is: $VERSION"
@@ -21,13 +21,13 @@ then
     if ([ $TYPE == "master" ] || [ $TYPE == "dev" ])
     then
         echo "Preparing to build version $TYPE"
-        grunt ci --build-target=$TYPE
+        npm run build:deploy $TYPE
 
         rm -rf "./gh-pages/$TYPE"
         cp -r "./public" "./gh-pages/$TYPE"
     fi
 
-    if ([ $TYPE == "master" ] || [ -n "$RELEASE_BRANCH" ])
+    if ([ "$TYPE" == "chore/deployable-build" ] || [ $TYPE == "master" ] || [ -n "$RELEASE_BRANCH" ])
     then
         echo "On $TYPE - building versioned build"
         if ([ -z "$VERSION" ])
@@ -37,13 +37,14 @@ then
         fi
         if [ -n "$RELEASE_BRANCH" ]
         then
-            #For release branches add rc postfix
+            # For release branches add rc postfix
             VERSION="$VERSION-rc"
             echo "Release branch - updating version to $VERSION"
         fi
         # Rebuild everything to do $VERSION
         echo "Cleaning build. Targetting $VERSION"
-        grunt ci --build-target=$VERSION
+        echo "$VERSION"
+        npm run build:deploy $VERSION
 
         rm -rf "./gh-pages/$VERSION"
         cp -r "./public" "./gh-pages/$VERSION"
@@ -51,13 +52,13 @@ then
 
     cd gh-pages
 
-    #Removing git history
+    # Removing git history
     rm -rf .git
     git init
 
-    # inside this git repo we'll pretend to be a new user
+    # Inside this git repo we'll pretend to be a new user
     git config user.name "Travis CI"
-    git config user.email "travis@scottlogic.com"
+    git config user.email "travis@rjmcneill.com"
 
     # The first and only commit to this new Git repo contains all the
     # files present with the commit message "Deploy to GitHub Pages".
@@ -72,7 +73,7 @@ then
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
     echo "Cleaning residual gh-pages folder"
-    rm -rf ./gh-pages
+    rm -rf ../gh-pages
 else
     echo "Nothing needs deploying"
 fi
