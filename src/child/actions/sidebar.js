7ad86ff2dc8af755f0054de6555346951aa6e80c
Robert McNeill <rjmcneill@users.noreply.github.com>
2016-07-21 15:12:11 +0100

    feat: move to a single parent store (#854)
    * feat: Moved to single parent store
    
    * refactor: use selectors for deriving data in map state to props
    
    * chore: remove unused dependency
    
    * Added windowName to new action types

--- src/child/actions/sidebar.js
+++ src/child/actions/sidebar.js
@@ -1,9 +1,10 @@
 import { SIDEBAR as ACTION_TYPES } from '../constants/actionTypes.js';
-
 import { search as quandlServiceSearch } from '../services/QuandlService.js';
+import currentWindowService from '../services/currentWindowService';
 
 export function searchInput(term) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SEARCH_INPUT,
         term
     };
@@ -11,6 +12,7 @@ export function searchInput(term) {
 
 export function selectStock(code, name) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SELECTION,
         code,
         name
@@ -19,12 +21,14 @@ export function selectStock(code, name) {
 
 export function unselectStock() {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.UNSELECT
     };
 }
 
 export function insertFavouriteAt(index, code) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.INSERT_FAVOURITE_AT,
         index,
         code
@@ -33,6 +37,7 @@ export function insertFavouriteAt(index, code) {
 
 export function toggleFavourite(code) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.TOGGLE_FAVOURITE,
         code
     };
@@ -40,18 +45,21 @@ export function toggleFavourite(code) {
 
 export function selectSearch() {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SEARCH_CLICKED
     };
 }
 
 export function selectFavourites() {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.FAV_CLICKED
     };
 }
 
 export function quandlResponse(code, name) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.QUANDL_RESPONSE,
         code,
         name
@@ -60,12 +68,14 @@ export function quandlResponse(code, name) {
 
 function clearSearch() {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.CLEAR_SEARCH
     };
 }
 
 function searchStarted(term) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SEARCH_STARTED,
         term
     };
@@ -73,6 +83,7 @@ function searchStarted(term) {
 
 function searchFinished(term, results) {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SEARCH_FINISHED,
         term,
         results
@@ -81,6 +92,7 @@ function searchFinished(term, results) {
 
 function searchError() {
     return {
+        windowName: currentWindowService.getCurrentWindow().name,
         type: ACTION_TYPES.SEARCH_ERROR
     };
 }
---
+++ src/parent/reducers/reducers.js
@@ -0,0 +1,42 @@
+import ACTION_TYPES from '../constants/actionTypes.js';
+import childReducers from '../../child/reducers/reducers.js';
+
+function parentReducer(state = {}, action) {
+
+    switch (action.type) {
+
+    case ACTION_TYPES.CLOSE: {
+        const newState = Object.assign({}, state);
+        delete newState[action.windowName];
+        return newState;
+    }
+
+    default: {
+        const subString = action.type.substring(0, 5);
+
+        // All child actions are prefaced with 'CHILD'; if the incoming
+        // action is for the child, let the child reducers handle the action
+        if (subString === 'CHILD') {
+            const childState = state[action.windowName];
+            let newState = {};
+
+            if (childState) {
+                newState = Object.assign({}, state, {
+                    [action.windowName]: childReducers(childState, action)
+                });
+            } else {
+                newState = Object.assign({}, state, {
+                    [action.windowName]: childReducers({}, action)
+                });
+            }
+
+            return newState;
+        }
+
+        return state;
+    }
+
+    }
+}
+
+export default parentReducer;
