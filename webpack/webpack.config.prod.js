4e4392ed391dc7009e3099379f783ae4bbd65152
James Leftley <jleftley@scottlogic.com>
2016-07-25 22:23:38 +0100

    chore: quieten webpack production build by disabling UglifyJS warnings

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -8,8 +8,12 @@ config.output.path = `${__dirname}/../public`;
 config.plugins.push(
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            warnings: false
+        }
     })
 );
 
 module.exports = config;
-
