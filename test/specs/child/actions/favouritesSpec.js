d631e00f5c699aae3c645d98b3a3ca8b0fc83395
James Leftley <jleft@users.noreply.github.com>
2016-09-04 09:58:13 +0100

    feat: drag favourite from window x to window y (#898)
    * feat: drag favourite from window x to window y
    
    * test: fix toggle favourite tests

--- test/specs/child/actions/favouritesSpec.js
+++ test/specs/child/actions/favouritesSpec.js
@@ -1,10 +1,12 @@
 import { expect } from 'chai';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import currentWindowServiceStub from '../../../helper/currentWindowServiceStub';
 import { insertFavouriteAt,
          toggleFavourite,
          quandlResponse,
          __RewireAPI__ as rewiredActions } from '../../../../src/child/actions/favourites';
-import { FAVOURITES as ACTION_TYPES } from '../../../../src/shared/constants/actionTypes';
+import { FAVOURITES as FAVOURITES_ACTION_TYPES, SELECTION as SELECTION_ACTION_TYPES } from '../../../../src/shared/constants/actionTypes';
 
 describe('child/actions/favourites', () => {
     before(() => {
@@ -20,23 +22,11 @@ describe('child/actions/favourites', () => {
         const code = 'GOOG';
         const expectedAction = {
             windowName: 'window0002',
-            type: ACTION_TYPES.INSERT_FAVOURITE_AT,
+            type: FAVOURITES_ACTION_TYPES.INSERT_FAVOURITE_AT,
             index,
             code
         };
         const actualAction = insertFavouriteAt(index, code);
-        expect(actualAction.type).to.be.a('string');
-        expect(actualAction).to.deep.equal(expectedAction);
-    });
-
-    it('should create an action to toggle a favourite', () => {
-        const code = 'GOOG';
-        const expectedAction = {
-            windowName: 'window0002',
-            type: ACTION_TYPES.TOGGLE_FAVOURITE,
-            code
-        };
-        const actualAction = toggleFavourite(code);
         expect(actualAction.type).to.be.a('string');
         expect(actualAction).to.deep.equal(expectedAction);
     });
@@ -46,12 +36,277 @@ describe('child/actions/favourites', () => {
         const name = 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume';
         const expectedAction = {
             windowName: 'window0002',
-            type: ACTION_TYPES.QUANDL_RESPONSE,
+            type: FAVOURITES_ACTION_TYPES.QUANDL_RESPONSE,
             code,
             name
         };
         const actualAction = quandlResponse(code, name);
         expect(actualAction.type).to.be.a('string');
         expect(actualAction).to.deep.equal(expectedAction);
+    });
+
+    describe('toggleFavourite', () => {
+        let store;
+        let createMockStore;
+
+        beforeEach(() => {
+            createMockStore = configureMockStore([thunk]);
+        });
+
+        afterEach(() => {
+            createMockStore = null;
+            store = null;
+        });
+
+        describe('determining window name', () => {
+            it('should default the window name to the current window name if it is not specified', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: [],
+                                names: {}
+                            },
+                            selection: {}
+                        }
+                    }
+                });
+
+                const code = 'AAPL';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE }
+                ];
+
+                store.dispatch(toggleFavourite(code));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+
+            it('should allow the window name to be specified', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0003: {
+                            favourites: {
+                                codes: [],
+                                names: {}
+                            },
+                            selection: {}
+                        }
+                    }
+                });
+
+                const code = 'AAPL';
+                const windowName = 'window0003';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE }
+                ];
+
+                store.dispatch(toggleFavourite(code, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+        });
+
+        describe('toggling a favourite', () => {
+            it('should create an action to toggle a stock if it is not the current selection and is not a favourite', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: [],
+                                names: {}
+                            },
+                            selection: {}
+                        }
+                    }
+                });
+
+                const code = 'AAPL';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE }
+                ];
+
+                store.dispatch(toggleFavourite(code, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+
+            it('should create an action to toggle a stock if it is not the current selection but is a favourite', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: ['AAPL', 'GOOG'],
+                                names: {
+                                    AAPL: 'Apple Inc (AAPL) Prices, Dividends, Splits and Trading Volume',
+                                    GOOG: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                                }
+                            },
+                            selection: {
+                                code: 'GOOG',
+                                name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                            }
+                        }
+                    }
+                });
+
+                const code = 'AAPL';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE }
+                ];
+
+                store.dispatch(toggleFavourite(code, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+
+            it('should create an action to toggle a stock if it is the current selection but is not a favourite', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: [],
+                                names: {}
+                            },
+                            selection: {
+                                code: 'GOOG',
+                                name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                            }
+                        }
+                    }
+                });
+
+                const code = 'GOOG';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE }
+                ];
+
+                store.dispatch(toggleFavourite(code, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+        });
+
+        describe('toggling a favourite and unselecting a stock', () => {
+            it('should create an action to toggle the selected stock and unselect the stock if it is the only favourited stock', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: ['GOOG'],
+                                names: { GOOG: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume' }
+                            },
+                            selection: {
+                                code: 'GOOG',
+                                name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                            }
+                        }
+                    }
+                });
+
+                const code = 'GOOG';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE },
+                    { windowName, type: SELECTION_ACTION_TYPES.UNSELECT }
+                ];
+
+                store.dispatch(toggleFavourite(code, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()[1].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+        });
+
+        describe('toggling a favourite and selecting a new stock', () => {
+            it('should create an action to toggle the selected stock and select the first stock in the favourites list, if there is one other favourite', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: ['AAPL', 'GOOG'],
+                                names: {
+                                    AAPL: 'Apple Inc (AAPL) Prices, Dividends, Splits and Trading Volume',
+                                    GOOG: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                                }
+                            },
+                            selection: {
+                                code: 'GOOG',
+                                name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                            }
+                        }
+                    }
+                });
+
+                const favouriteToToggleCode = 'GOOG';
+                const favouriteToSelectCode = 'AAPL';
+                const favouriteToSelectName = 'Apple Inc (AAPL) Prices, Dividends, Splits and Trading Volume';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code: favouriteToToggleCode, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE },
+                    { windowName, code: favouriteToSelectCode, name: favouriteToSelectName, type: SELECTION_ACTION_TYPES.SELECTION }
+                ];
+
+                store.dispatch(toggleFavourite(favouriteToToggleCode, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()[1].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+
+            it('should create an action to toggle the selected stock and select the first stock in the favourites list, if there are other favourites', () => {
+                store = createMockStore({
+                    childWindows: {
+                        window0002: {
+                            favourites: {
+                                codes: ['AAPL', 'ABCD', 'GOOG'],
+                                names: {
+                                    AAPL: 'Apple Inc (AAPL) Prices, Dividends, Splits and Trading Volume',
+                                    ABCD: 'Another stock',
+                                    GOOG: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                                }
+                            },
+                            selection: {
+                                code: 'GOOG',
+                                name: 'Alphabet Inc (GOOG) Prices, Dividends, Splits and Trading Volume'
+                            }
+                        }
+                    }
+                });
+
+                const favouriteToToggleCode = 'GOOG';
+                const favouriteToSelectCode = 'AAPL';
+                const favouriteToSelectName = 'Apple Inc (AAPL) Prices, Dividends, Splits and Trading Volume';
+                const windowName = 'window0002';
+
+                const expectedActions = [
+                    { windowName, code: favouriteToToggleCode, type: FAVOURITES_ACTION_TYPES.TOGGLE_FAVOURITE },
+                    { windowName, code: favouriteToSelectCode, name: favouriteToSelectName, type: SELECTION_ACTION_TYPES.SELECTION }
+                ];
+
+                store.dispatch(toggleFavourite(favouriteToToggleCode, windowName));
+
+                expect(store.getActions()[0].type).to.be.a('string');
+                expect(store.getActions()[1].type).to.be.a('string');
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            });
+        });
     });
 });
