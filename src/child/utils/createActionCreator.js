aa3da29d68c7b8f63d818eaa5ad765f2215bb22e
James Leftley <jleft@users.noreply.github.com>
2016-09-08 22:54:52 +0100

    fix: select/unselect stock in correct window when moving stock between windows (#904)

--- src/child/utils/createActionCreator.js
+++ src/child/utils/createActionCreator.js
@@ -1,5 +1,6 @@
 import currentWindowService from '../services/currentWindowService';
+import defaults from 'lodash.defaults';
 
-export default (actionCreator) => (...args) => Object.assign({}, actionCreator(...args), {
-    windowName: currentWindowService.getCurrentWindowName()
-});
+// Returns an action creator which will provide a default windowName property, set to the current window's name,
+// unless the windowName is set by the original action creator
+export default (actionCreator) => (...args) => defaults(actionCreator(...args), { windowName: currentWindowService.getCurrentWindowName() });
