51ccc4d8cdf74018ec9941251157539aadde8371
James Leftley <jleft@users.noreply.github.com>
2016-09-27 11:32:56 +0100

    feat: select initial stock when dropping tear-out window (#912)

--- src/child/reducers/selection.js
+++ src/child/reducers/selection.js
@@ -1,14 +1,21 @@
-import { SELECTION as ACTION_TYPES } from '../../shared/constants/actionTypes';
+import { SELECTION as SELECTION_ACTION_TYPES, FAVOURITES as FAVOURITES_ACTION_TYPES } from '../../shared/constants/actionTypes';
 
 export default function selection(state = {}, action) {
     switch (action.type) {
-    case ACTION_TYPES.SELECTION:
+    case SELECTION_ACTION_TYPES.SELECTION:
         return Object.assign({}, state, {
             code: action.code,
-            name: action.name,
+            name: action.name
         });
-    case ACTION_TYPES.UNSELECT:
+    case SELECTION_ACTION_TYPES.UNSELECT:
         return {};
+    case FAVOURITES_ACTION_TYPES.QUANDL_RESPONSE:
+        if (state.code === action.code) {
+            return Object.assign({}, state, {
+                name: action.name
+            });
+        }
+        return state;
     default:
         return state;
     }
