7202a23bb7410d9fc48090cb0adb784a539ca39b
James Leftley <jleft@users.noreply.github.com>
2016-10-31 22:10:49 +0000

    chore: move OpenFin launcher scripts to own directory (#928)

--- openfin-dev-launcher.js
+++
@@ -1,9 +0,0 @@
-const openfinLauncher = require('openfin-launcher');
-
-openfinLauncher.launchOpenFin({
-    configPath: 'http://localhost:5000/app.json'
-})
-.then(() => {
-    console.log('success!');
-})
-.catch(console.error);
