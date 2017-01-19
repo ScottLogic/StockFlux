4d2a1fe89005b22d0a492c00651e5a9117a9d54c
James Leftley <jleft@users.noreply.github.com>
2017-01-19 22:03:18 +0000

    chore: Add Quandl deployment key (#936)

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -8,7 +8,12 @@ const config = Object.assign({}, sharedConfig);
 config.output.path = `${__dirname}/../public`;
 config.plugins.push(
     new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
+        'process.env.NODE_ENV': '"production"',
+        'process.env.TRAVIS': JSON.stringify(process.env.TRAVIS),
+        'process.env.TRAVIS_SECURE_ENV_VARS': JSON.stringify(process.env.TRAVIS_SECURE_ENV_VARS),
+        'process.env.TRAVIS_PULL_REQUEST': JSON.stringify(process.env.TRAVIS_PULL_REQUEST),
+        'process.env.QUANDL_API_KEY': JSON.stringify(process.env.QUANDL_API_KEY),
+        'process.env.QUANDL_KEY': JSON.stringify(process.env.QUANDL_KEY)
     }),
     new webpack.optimize.UglifyJsPlugin({
         compressor: {
