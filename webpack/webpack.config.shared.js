08e855f5f98afec44a113b927e8e5552271bd813
Chris Wilton <cwilton@scottlogic.co.uk>
2019-01-04 17:01:37 +0000

    Remove unnecessary webpack loader for openfin-layouts

--- webpack/webpack.config.shared.js
+++ webpack/webpack.config.shared.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules(?!(\/|\\)openfin-layouts)/, loader: 'babel-loader' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
             { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
