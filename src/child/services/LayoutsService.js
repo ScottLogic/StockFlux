bdb0b52d6d4babf622ac2abe628a863feccf3f09
Michael Boyd <mboyd@scottlogic.com>
2019-04-10 11:46:38 +0100

    refactor: use openfin-layouts dock events

--- src/child/services/LayoutsService.js
+++ src/child/services/LayoutsService.js
@@ -1,27 +1,13 @@
 import { snapAndDock } from 'openfin-layouts';
 import { joinedSnapGroup, leftSnapGroup } from '../actions/window';
 
-const { name: childWindowName, uuid } = fin.desktop.Window.getCurrent();
-
 export async function start(store) {
     await snapAndDock.addEventListener('window-docked', () => {
-        fin.desktop.InterApplicationBus.send(uuid, 'join-snap-group', null);
+        store.dispatch(joinedSnapGroup());
     });
 
     await snapAndDock.addEventListener('window-undocked', () => {
-        fin.desktop.InterApplicationBus.send(uuid, 'leave-snap-group', null);
-    });
-
-    fin.desktop.InterApplicationBus.subscribe(uuid, 'joined-snap-group', ({ windowName }) => {
-        if (childWindowName === windowName) {
-            store.dispatch(joinedSnapGroup());
-        }
-    });
-
-    fin.desktop.InterApplicationBus.subscribe(uuid, 'left-snap-group', ({ windowName }) => {
-        if (childWindowName === windowName) {
-            store.dispatch(leftSnapGroup());
-        }
+        store.dispatch(leftSnapGroup());
     });
 }
 
