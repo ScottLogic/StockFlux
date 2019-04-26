e007679a37ffc9f50d4bc9c7aa1ac2b27b1dbe95
Dean Kerr <dkerr@scottlogic.co.uk>
2019-04-26 13:15:39 +0100

    Remove references to scrollbar plugin in stockflux-components

--- stockflux-components/src/index.js
+++ stockflux-components/src/index.js
@@ -2,8 +2,6 @@ import './index.css';
 import Titlebar from './components/Titlebar';
 import AllImages from './styles/png';
 
-import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar';
-import 'jquery-mousewheel/jquery.mousewheel';
 
 export default {
   Titlebar,
