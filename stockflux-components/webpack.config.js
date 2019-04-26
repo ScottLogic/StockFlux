654596687dd613c2a990de622bfa0624a8663225
Dean Kerr <dkerr@scottlogic.co.uk>
2019-04-26 12:09:57 +0100

    Add build configuration for stockflux-components library

---
+++ stockflux-components/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development', // TODO: production mode
+    entry: path.resolve('src', 'index.js'),
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'stockflux-components',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                use: [
+                    { loader: 'babel-loader' }
+                ]
+            },
+            {
+                test: /\.css$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.png$/,
+                include: [
+                    path.resolve(__dirname, 'src')
+                ],
+                use: [
+                    { loader: 'file-loader' }
+                ]
+            }
+        ],
+    },
+    externals: {
+        react: 'react',
+        'openfin-react-hooks': 'openfin-react-hooks',
+        'openfin-layouts': 'openfin-layouts',
+    }
+};
