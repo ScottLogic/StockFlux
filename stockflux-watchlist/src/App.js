13d11dfc1caba9c5e79b0182a6313153503d112e
Auguste Bagdonaite <abagdonaite@scottlogic.com>
2019-05-06 15:14:20 +0100

    Removed redundant export

--- stockflux-watchlist/src/App.js
+++ stockflux-watchlist/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Watchlist } from "./components/Watchlist/Watchlist";
-import Components from "stockflux-components";
-import "./App.css";
+import React from 'react';
+import Watchlist from './components/Watchlist/Watchlist';
+import Components from 'stockflux-components';
+import './App.css';
 
 const App = () => {
-  return (
-    <>
-      <Components.Titlebar />
-      <Watchlist />
-    </>
-  );
+	return (
+		<>
+			<Components.Titlebar />
+			<Watchlist />
+		</>
+	);
 };
 
 export default App;
