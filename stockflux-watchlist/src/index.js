24f0e7efe95fcfe6733e8ec6bdd970551aa63f26
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-10 10:41:47 +0100

    Remove malihu custom scrollbar plugin usage from watchlist

--- stockflux-watchlist/src/index.js
+++ stockflux-watchlist/src/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar";
-import "malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css";
-import "jquery-mousewheel/jquery.mousewheel";
-
 const mountApp = () => {
   ReactDOM.render(<App />, document.getElementById("root"));
 };
