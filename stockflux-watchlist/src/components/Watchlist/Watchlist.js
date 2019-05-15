e0c1ef6c469045956e8305e9aa9220b8d987955d
Auguste Bagdonaite <abagdonaite@scottlogic.com>
2019-05-15 11:17:23 +0100

    FDC3 Intent Listener for intents raised by the search app against watchlist app

--- stockflux-watchlist/src/components/Watchlist/Watchlist.js
+++ stockflux-watchlist/src/components/Watchlist/Watchlist.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WatchlistCard from '../WatchlistCard/WatchlistCard';
 import Components from 'stockflux-components';
 import * as fdc3 from 'openfin-fdc3';
@@ -10,13 +10,34 @@ function Watchlist() {
   const [dragOverIndex, setDragOverIndex] = useState(null);
   const [dragStartClientY, setDragStartClientY] = useState(null);
   const [cardHeight, setCardHeight] = useState(null);
-  const [watchlist, setWatchlist] = useState([
-    'AAPL',
-    'AAP',
-    'CC',
-    'MS',
-    'JPS'
-  ]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [isInitialised, setIsInitialised] = useState(false);
+
+  useEffect(() => {
+    setIsInitialised(false);
+    const initialWatchlist = ['AAPL', 'AAP', 'CC', 'MS', 'JPS'];
+    const localStorageWatchlist = JSON.parse(localStorage.getItem('watchlist'));
+    const tempWatchlist =
+      !localStorageWatchlist || localStorageWatchlist.length === 0
+        ? initialWatchlist
+        : localStorageWatchlist;
+    persistWatchlist(tempWatchlist);
+    setIsInitialised(true);
+  }, []);
+
+  useEffect(() => {
+    fdc3.addIntentListener('WatchlistAdd', context => {
+      if (context && context.name) {
+        const newSymbol = context.name;
+        persistWatchlist([newSymbol, ...watchlist]);
+      }
+    });
+  }, [watchlist]);
+
+  const persistWatchlist = watchlistToPersist => {
+    setWatchlist(watchlistToPersist);
+    localStorage.setItem('watchlist', JSON.stringify(watchlistToPersist));
+  };
 
   const onIconClick = symbol => {
     return e => {
@@ -29,7 +50,7 @@ function Watchlist() {
     setUnwatchedSymbol(null);
     const symbolIndex = getSymbolIndex(symbol);
     watchlist.splice(symbolIndex, 1);
-    setWatchlist(watchlist);
+    persistWatchlist(watchlist);
   };
 
   const onModalBackdropClick = e => {
@@ -95,9 +116,9 @@ function Watchlist() {
         0,
         symbol
       );
-      setWatchlist(tempWatchlist);
+      persistWatchlist(tempWatchlist);
     } else if (!watchlist.includes(symbol)) {
-      setWatchlist(watchlist.push(symbol));
+      persistWatchlist(watchlist.push(symbol));
     }
   };
 
@@ -145,25 +166,28 @@ function Watchlist() {
           &nbsp;
         </div>
       </div>
-      <Components.ScrollWrapperY contentChanged={unwatchedSymbol}>
-        {watchlist.length === 0 && (
+      <Components.ScrollWrapperY
+        contentChanged={unwatchedSymbol || isInitialised}
+      >
+        {watchlist.length === 0 ? (
           <div className="no-watchlist">
-            <p>You have no watchlist stocks to display.</p>
-            <p>Use the search tab to add new stocks to the list.</p>
+            <p>You have no stocks to display.</p>
+            <p>Use stockflux search app to add new stocks to the list.</p>
           </div>
+        ) : (
+          watchlist.map((symbol, i) => (
+            <WatchlistCard
+              key={symbol}
+              symbol={symbol}
+              bindings={bindings}
+              isUnwatching={unwatchedSymbol === symbol}
+              dragOver={dragOverIndex === i}
+              dragOverBottom={
+                dragOverIndex === watchlist.length && i === watchlist.length - 1
+              }
+            />
+          ))
         )}
-        {watchlist.map((symbol, i) => (
-          <WatchlistCard
-            key={symbol}
-            symbol={symbol}
-            bindings={bindings}
-            isUnwatching={unwatchedSymbol === symbol}
-            dragOver={dragOverIndex === i}
-            dragOverBottom={
-              dragOverIndex === watchlist.length && i === watchlist.length - 1
-            }
-          />
-        ))}
       </Components.ScrollWrapperY>
     </div>
   );
