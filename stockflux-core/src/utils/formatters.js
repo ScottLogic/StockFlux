e9458d5d23df2f9ebc8248612ce5a6fb39923852
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-15 16:36:35 +0100

    Use stockflux-core formatter

--- stockflux-core/src/utils/formatters.js
+++ stockflux-core/src/utils/formatters.js
@@ -1,9 +1,4 @@
 export const truncate = (input) => {
-    if (input) {
-        const openBracketIndex = input.indexOf('(');
-        if (openBracketIndex !== -1) {
-            return input.slice(0, openBracketIndex - 1); // Also trim the space before the bracket
-        }
-    }
-    return input;
+    const openBracketIndex = input.indexOf('(');
+    return openBracketIndex === -1 ? input : input.slice(0, openBracketIndex - 1);
 };
