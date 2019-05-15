97ee3f1109e947e52fa942f7ab6be6015209b815
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-15 11:04:41 +0100

    Fire watchlist add intent when add to watchlist button clicked

--- stockflux-search/src/search-result/SearchResult.js
+++ stockflux-search/src/search-result/SearchResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
+import * as fdc3 from 'openfin-fdc3';
 
 import styles from './SearchResult.module.css';
 
@@ -8,12 +9,22 @@ const format = name => {
     return openBracketIndex === -1 ? name : name.slice(0, openBracketIndex - 1);
 };
 
+const handleWatchlistAddClick = (code, name) => {
+    fdc3.raiseIntent('WatchlistAdd', {
+        type: 'security',
+        name,
+        id: {
+            default: code
+        }
+    });
+};
+
 const SearchResult = ({ code, name }) => (
     <div className={styles.searchResult}>
         <div className={styles.name}>{format(name)}</div>
         <div className={styles.subtitle}>{code}</div>
         <div className={styles.containerActions}>
-            <button type="button" className={styles.buttonAction}>Add to Watchlist</button>
+            <button type="button" className={styles.buttonAction} onClick={() => handleWatchlistAddClick(code, name)}>Add to Watchlist</button>
             <button type="button" className={styles.buttonAction}>Add to Chart</button>
         </div>
     </div>
