3d6eba4f3162990caf01c57ab5944ee036e2ab1f
Dean Kerr <dkerr@scottlogic.co.uk>
2019-05-30 17:19:59 +0100

    Use simpler close button logic

--- stockflux-launcher/src/toolbar/CloseButton.js
+++ stockflux-launcher/src/toolbar/CloseButton.js
@@ -5,11 +5,7 @@ import './CloseButton.css';
 export default (
   <button
     className="close"
-    onClick={() => {
-      // eslint-disable-next-line no-undef
-      var application = fin.desktop.Application.getCurrent();
-      application.close();
-    }}
+    onClick={() => window.fin.Window.getCurrentSync().close()}
   >
     <FaTimes />
   </button>
