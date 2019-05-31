e057e90c7fa902ca0b68efa94c3a4250dedda3d2
Jack Graham <jackmgraham@hotmail.co.uk>
2019-05-31 09:43:08 +0100

    Add icons to search results

--- stockflux-launcher/src/free-text-search/search-result/SearchResult.js
+++ stockflux-launcher/src/free-text-search/search-result/SearchResult.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as fdc3 from 'openfin-fdc3';
+import { FaChartLine, FaRegListAlt  } from 'react-icons/fa'
 import * as PropTypes from 'prop-types';
 import { Utils, Intents } from 'stockflux-core';
 
@@ -27,14 +28,14 @@ const SearchResult = ({ code, name }) => (
         className="buttonAction"
         onClick={() => handleWatchlistAddClick(code, name)}
       >
-        Add to Watchlist
+        <FaRegListAlt className="buttonIcon"/>
       </button>
       <button
         type="button"
         className="buttonAction"
         onClick={() => handleChartAddClick(code, name)}
       >
-        Add to Chart
+        <FaChartLine className="buttonIcon"/>
       </button>
     </div>
   </div>
