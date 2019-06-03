8538d4196a2c606e3fe9580c264188a236640966
Dean Kerr <dkerr@scottlogic.co.uk>
2019-06-03 16:14:30 +0100

    Fix issue with click handlers within search results child window not firing

--- stockflux-core/src/intents/viewChart.js
+++ stockflux-core/src/intents/viewChart.js
@@ -1,21 +1,21 @@
 import * as fdc3 from 'openfin-fdc3';
 
-export const viewChart = async function(symbol, stockName) {
-  const availableApps = await fdc3.findIntent(fdc3.Intents.VIEW_CHART);
-  if (availableApps && availableApps.apps) {
-    const chart = availableApps.apps.find(app => app.appId === 'stockflux-chart-container');
-    if (chart) {
-      await fdc3.raiseIntent(
-        fdc3.Intents.VIEW_CHART,
-        {
-          type: 'security',
-          name: symbol,
-          id: {
-            default: stockName
-          }
-        },
-        chart.name
-      );
+export default async (symbol, stockName) => {
+    const availableApps = await fdc3.findIntent(fdc3.Intents.VIEW_CHART);
+    if (availableApps && availableApps.apps) {
+        const chart = availableApps.apps.find(app => app.appId === 'stockflux-chart-container');
+        if (chart) {
+            await fdc3.raiseIntent(
+                fdc3.Intents.VIEW_CHART,
+                {
+                    type: 'security',
+                    name: symbol,
+                    id: {
+                        default: stockName
+                    }
+                },
+                chart.name
+            );
+        }
     }
-  }
 };
--- stockflux-launcher/src/free-text-search/search-result/SearchResult.js
+++ stockflux-launcher/src/free-text-search/search-result/SearchResult.js
@@ -1,48 +1,22 @@
 import React from 'react';
-import * as fdc3 from 'openfin-fdc3';
 import { FaChartLine, FaRegListAlt, FaNewspaper  } from 'react-icons/fa'
 import * as PropTypes from 'prop-types';
-import { Utils, Intents } from 'stockflux-core';
+import { Utils } from 'stockflux-core';
 
-const handleWatchlistAddClick = (code, name) => {
-  fdc3.raiseIntent('WatchlistAdd', {
-    type: 'security',
-    name,
-    id: {
-      default: code
-    }
-  });
-};
-
-const handleViewNewsClick = async (code) => {
-  Intents.viewNews(code);
-}
-
+// Add unique classes onto buttons to allow events listeners to be attached within child window
 const SearchResult = ({ code, name }) => (
   <div className="searchResult">
     <div className="name">{Utils.truncate(name)}</div>
     <div className="subtitle">{code}</div>
     <div className="containerActions">
-      <button
-        type="button"
-        className="buttonAction"
-        onClick={() => handleViewNewsClick(code)}
-      >
-        <FaNewspaper className="buttonIcon"/>
+      <button type="button" className="buttonAction newsView">
+        <FaNewspaper className="buttonIcon" />
       </button>
-      <button
-        type="button"
-        className="buttonAction"
-        onClick={() => handleWatchlistAddClick(code, name)}
-      >
-        <FaRegListAlt className="buttonIcon"/>
+      <button type="button" className="buttonAction watchlistAdd">
+        <FaRegListAlt className="buttonIcon" />
       </button>
-      <button
-        type="button"
-        className="buttonAction"
-        onClick={() => Intents.viewChart(code, name)}
-      >
-        <FaChartLine className="buttonIcon"/>
+      <button type="button" className="buttonAction chartView">
+        <FaChartLine className="buttonIcon" />
       </button>
     </div>
   </div>
