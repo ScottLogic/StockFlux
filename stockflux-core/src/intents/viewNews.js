8dc9df607b10778f3a62bd21ae6fd76dd390ecd0
Jack Graham <jackmgraham@hotmail.co.uk>
2019-06-06 14:24:47 +0100

    Rename chart-container to container

--- stockflux-core/src/intents/viewNews.js
+++ stockflux-core/src/intents/viewNews.js
@@ -3,7 +3,7 @@ import * as fdc3 from 'openfin-fdc3';
 export default async (symbol) => {
     const availableApps = await fdc3.findIntent('ViewNews');
     if (availableApps && availableApps.apps) {
-        const newsApp = availableApps.apps.find(app => app.appId === 'stockflux-chart-container');
+        const newsApp = availableApps.apps.find(app => app.appId === 'stockflux-container');
         if (newsApp) {
             await fdc3.raiseIntent(
                 'ViewNews',
