9e8871c04c9cf2d954417b5ee10eb0e6adace21c
Dean Kerr <dkerr@scottlogic.co.uk>
2019-06-25 09:49:59 +0100

    Remove filtering of intents to allow non-SL apps to use same intent

--- stockflux-core/src/intents/viewChart.js
+++ stockflux-core/src/intents/viewChart.js
@@ -3,23 +3,15 @@ import * as fdc3 from 'openfin-fdc3';
 const defaultName = 'Apple Inc.';
 const defaultSymbol = 'AAPL';
 
-export default async (symbol, stockName) => {
-    const availableApps = await fdc3.findIntent(fdc3.Intents.VIEW_CHART);
-    if (availableApps && availableApps.apps) {
-        const chart = availableApps.apps.find(app => app.appId === 'stockflux-container');
-        if (chart) {
-            await fdc3.raiseIntent(
-                fdc3.Intents.VIEW_CHART,
-                {
-                    type: 'security',
-                    name: symbol || defaultSymbol,
-                    appName: 'stockflux-chart',
-                    id: {
-                        default: stockName || defaultName,
-                    }
-                },
-                chart.name
-            );
+export default async (symbol, stockName) =>
+    fdc3.raiseIntent(
+        fdc3.Intents.VIEW_CHART,
+        {
+            type: 'security',
+            name: symbol || defaultSymbol,
+            appName: 'stockflux-chart',
+            id: {
+                default: stockName || defaultName,
+            }
         }
-    }
-};
+    );
--- stockflux-core/src/intents/viewNews.js
+++ stockflux-core/src/intents/viewNews.js
@@ -2,20 +2,12 @@ import * as fdc3 from 'openfin-fdc3';
 
 const defaultSymbol = 'AAPL';
 
-export default async (symbol) => {
-    const availableApps = await fdc3.findIntent('ViewNews');
-    if (availableApps && availableApps.apps) {
-        const newsApp = availableApps.apps.find(app => app.appId === 'stockflux-container');
-        if (newsApp) {
-            await fdc3.raiseIntent(
-                'ViewNews',
-                {
-                    type: 'news',
-                    name: symbol || defaultSymbol,
-                    appName: 'stockflux-news'
-                },
-                newsApp.name
-            );
+export default async (symbol) =>
+    fdc3.raiseIntent(
+        'ViewNews',
+        {
+            type: 'news',
+            name: symbol || defaultSymbol,
+            appName: 'stockflux-news'
         }
-    }
-};
+    );
