f28a88c1478f37cebfcb9be6d3c229f6c6044b5e
Max Ovenden <scottcashreal@gmail.com>
2019-07-09 10:35:39 +0100

    added dummy server, screen for when no there are no transactions and the loading spinner functionality

--- stockflux-securities-master/src/components/SecuritiesTable.js
+++ stockflux-securities-master/src/components/SecuritiesTable.js
@@ -1,68 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Components from "stockflux-components";
 import "./SecuritiesTable.css";
 
-// hard coded data to be replaced with data from backend
-const SECURITIES_DATA = [
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." },
-  { exch: "LSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "LSE", symbol: "TSLA", name: "Tesla inc." },
-  { exch: "NYSE", symbol: "AAPL", name: "Apple inc." },
-  { exch: "STFX", symbol: "SCOTT", name: "Scott Logic inc." }
-];
+async function getSecuritiesData() {
+  const response = await fetch("http://ws00128:3001/securities");
+  const securities = await response.json();
+  return securities;
+}
 
 const SecuritiesTable = () => {
+  const [securitiesData, setSecuritiesData] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getSecuritiesData().then(securities => {
+      setIsLoading(false);
+      setSecuritiesData(securities);
+    });
+    getSecuritiesData().catch(() => {
+      setIsLoading(false);
+    })
+  }, []);
+
   return (
     <div className="securities-container">
-      <div className="securities-title">My Securities Table</div>
-      <div className="header-container">
-        <div className="securities-table-header">Exchange</div>
-        <div className="securities-table-header">Symbol</div>
-        <div className="securities-table-header">Name</div>
-      </div>
-      <div className="table-body">
-        <Components.ScrollWrapperY>
-          {SECURITIES_DATA.map((item, index) => (
-            <div key={index} className="securities-table-row">
-              <div className="securities-exchange-data">{item.exch}</div>
-              <div className="securities-symbol-data">{item.symbol}</div>
-              <div className="securities-name-data">{item.name}</div>
+        <>
+          <div className="securities-title">My Securities Table</div>
+          <div className="header-container">
+            <div className="securities-table-header">Exchange</div>
+            <div className="securities-table-header">Symbol</div>
+            <div className="securities-table-header">Name</div>
+          </div>
+          {isLoading ? <Components.LargeSpinner /> :
+          <div className="table-body">
+          {securitiesData.length > 0 ? (
+            <Components.ScrollWrapperY>
+              {securitiesData.map((item, index) => (
+                <div key={index} className="securities-table-row">
+                  <div className="securities-exchange-data">
+                    {item.securityId}
+                  </div>
+                  <div className="securities-symbol-data">{item.symbol}</div>
+                  <div className="securities-name-data">{item.name}</div>
+                </div>
+              ))}
+            </Components.ScrollWrapperY>
+          ) : (
+            <>
+            <div className="no-securities-container">
+              <div className="no-securities-message">You have no securities to show</div>
+              <div className="add-security-button"><button>Click to add security</button></div>
             </div>
-          ))}
-        </Components.ScrollWrapperY>
-      </div>
+            </>
+          )}
+          </div>
+        }
+        </>
     </div>
   );
 };
