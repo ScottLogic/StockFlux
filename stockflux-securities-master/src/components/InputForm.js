73ce054d3f5ca6a6b886954e7cdf8721f2578d5d
Max Ovenden <scottcashreal@gmail.com>
2019-07-16 11:58:48 +0100

    moved input values form using ref to using state and submitting the form logs the security object to the console

--- stockflux-securities-master/src/components/InputForm.js
+++ stockflux-securities-master/src/components/InputForm.js
@@ -1,21 +1,21 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./InputForm.css";
 
 const InputForm = () => {
-  const nameRef = useRef(null);
-  const exchangeRef = useRef(null);
-  const symbolRef = useRef(null);
-  const isShownRef = useRef(true);
-  const isVisibleRef = useRef(true);
+  const [name, setName] = useState('');
+  const [exchange, setExchange] = useState('');
+  const [symbol, setSymbol] = useState('');
+  const [isShown, setIsShown] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(false);
 
   const submitForm = () => {
     const securityObject = {
-      name: nameRef.current,
-      exchange: exchangeRef.current,
-      symbol: symbolRef.current,
-      isShown: isShownRef.current,
-      isVisible: isVisibleRef.current
+      name: name,
+      exchange: exchange,
+      symbol: symbol,
+      isShown: isShown,
+      isEnabled: isEnabled
     };
     console.log(securityObject);
   };
@@ -32,7 +32,8 @@ const InputForm = () => {
             required
             className="input-form-input"
             id="name-input"
-            ref={nameRef}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
         <div className="input-row">
@@ -43,7 +44,8 @@ const InputForm = () => {
             required
             className="input-form-input"
             id="exchange-input"
-            ref={exchangeRef}
+            value={exchange}
+            onChange={(event) => setExchange(event.target.value)}
           />
         </div>
         <div className="input-row">
@@ -54,7 +56,8 @@ const InputForm = () => {
             required
             className="input-form-input"
             id="symbol-input"
-            ref={symbolRef}
+            value={symbol}
+            onChange={(event) => setSymbol(event.target.value)}
           />
         </div>
         <div className="input-checkbox-container">
@@ -64,8 +67,9 @@ const InputForm = () => {
           <input
             className="input-form-toggle"
             id="is-shown-toggle"
-            ref={isShownRef}
+            value={isShown}
             type="checkbox"
+            onChange={() => setIsShown(!isShown)}
           />
           <label className="toggle-switch" for="is-shown-toggle" />
         </div>
@@ -76,18 +80,18 @@ const InputForm = () => {
           <input
             className="input-form-toggle"
             id="is-enabled-toggle"
-            ref={isVisibleRef}
+            value={isEnabled}
             type="checkbox"
+            onChange={() => setIsEnabled(!isEnabled)}
           />
           <label className="toggle-switch" for="is-enabled-toggle" />
         </div>
         <div className="input-submit-button-container">
           <div className="input-submit-button">
-            <button type="submit">Create</button>
+            <button>Create</button>
           </div>
         </div>
       </form>
-
       <Link to="/">
         <div className="back-button">
           <button>
