960543b2593a042179371ddda80d3268d7219d6d
Max Ovenden <scottcashreal@gmail.com>
2019-07-23 15:12:47 +0100

    css classname changes in message component

--- stockflux-securities-master/src/components/Message.js
+++ stockflux-securities-master/src/components/Message.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Message.css";
 
 const Message = ({ message, type }) => {
-  return <div className={`securities-${type}-message`}>{message}</div>;
+  return <div className={`securities-message ${type}`}>{message}</div>;
 };
 
 export default Message;
