37b88562c1659b57286ffd205fc5c35e68c53582
Auguste Bagdonaite <abagdonaite@scottlogic.com>
2019-07-30 15:39:37 +0100

    Remove unused exports and update Readme

--- stockflux-components/src/index.js
+++ stockflux-components/src/index.js
@@ -1,9 +1,6 @@
 import Titlebar from './components/Titlebar/Titlebar';
 import ScrollWrapperY from './components/scroll-wrapper-y/ScrollWrapperY';
 import { Spinner, LargeSpinner } from './components/spinner/Spinner';
-import News from './components/icons/news.svg';
-import Chart from './components/icons/chart.svg';
-import Watchlist from './components/icons/watchlist.svg';
 import WatchlistShortcut from './components/app-launch-buttons/watchlist-shortcut/WatchlistShortcut';
 import ChartShortcut from './components/app-launch-buttons/chart-shortcut/ChartShortcut';
 import NewsShortcut from './components/app-launch-buttons/news-shortcut/NewsShortcut';
@@ -19,9 +16,6 @@ export default {
   ScrollWrapperY,
   Spinner,
   LargeSpinner,
-  News,
-  Chart,
-  Watchlist,
   WatchlistShortcut,
   ChartShortcut,
   NewsShortcut
