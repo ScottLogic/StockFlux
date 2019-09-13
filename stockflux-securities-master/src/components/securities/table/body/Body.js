18e406ea4324fc48a7f239ce7a31f6bccbaddd96
abscottlogic <abagdonaite@scottlogic.com>
2019-09-13 11:01:58 +0100

    Make window resizable

--- stockflux-securities-master/src/components/securities/table/body/Body.js
+++ stockflux-securities-master/src/components/securities/table/body/Body.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Components from 'stockflux-components';
 import SecurityShape from '../../../../shapes/Security';
 import Row from '../row/Row';
 import './Body.css';
 
 const Body = ({ securities, dispatch, fetchSecurities }) => (
-  <div className="tbody scrollable">
-    {securities &&
-      securities.length > 0 &&
-      securities.map(security => (
-        <Row
-          key={security.securityId}
-          security={security}
-          dispatch={dispatch}
-          fetchSecurities={fetchSecurities}
-        />
-      ))}
-  </div>
+  <Components.ScrollWrapperY>
+    <div className="tbody">
+      {securities &&
+        securities.length > 0 &&
+        securities.map(security => (
+          <Row
+            key={security.securityId}
+            security={security}
+            dispatch={dispatch}
+            fetchSecurities={fetchSecurities}
+          />
+        ))}
+    </div>
+  </Components.ScrollWrapperY>
 );
 
 Body.propTypes = {
