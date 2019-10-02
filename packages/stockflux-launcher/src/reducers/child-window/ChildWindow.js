6ca6918b4c2698ff9d16e907177504ce5b492822
abscottlogic <abagdonaite@scottlogic.com>
2019-10-02 10:00:47 +0100

    Simplify errors in ChildWindowState

--- packages/stockflux-launcher/src/reducers/child-window/ChildWindow.js
+++ packages/stockflux-launcher/src/reducers/child-window/ChildWindow.js
@@ -3,21 +3,11 @@ import childWindowState from './State';
 export const initialChildWindowState = childWindowState.initial;
 
 export default (state, action) => {
-  let errors;
-
-  const addError = error => (errors += error);
-
   if (!Object.keys(childWindowState).indexOf(action.type))
-    addError(`Invalid action type: ${action.type}. `);
-
-  if (action.type === childWindowState.error) {
+    throw new Error(`Invalid action type: ${action.type}.`);
+  else if (action.type === childWindowState.error) {
     console.error(action.error);
-    addError(`Error occured while dispatching action: ${action.type}. `);
+    throw new Error(`Error occured while dispatching action: ${action.type}.`);
   }
-
-  if (errors) {
-    throw new Error(errors);
-  }
-
   return action.type;
 };
