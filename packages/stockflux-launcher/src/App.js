7e3337b1a3c971c96af70b8e9ebcf40f16d39de3
kangelSL <51117162+kangelSL@users.noreply.github.com>
2019-10-08 16:26:19 +0100

    Feat: initial change for StockFlux dock/undock and launch changes

--- packages/stockflux-launcher/src/App.js
+++ packages/stockflux-launcher/src/App.js
@@ -1,22 +1,24 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useCallback, useRef, useState } from 'react';
 import cx from 'classnames';
 import { OpenfinApiHelpers } from 'stockflux-core';
-import { useDockWindow, ScreenEdge } from 'openfin-react-hooks';
+import { useDockWindow, useOptions, ScreenEdge } from 'openfin-react-hooks';
 import {
   FaChevronUp,
   FaChevronLeft,
   FaChevronRight,
-  FaRegHandRock
+  FaRegHandRock,
+  FaUnlock
 } from 'react-icons/fa';
-import AppShortcuts from './app-shortcuts/AppShortcuts';
 import FreeTextSearch from './free-text-search/FreeTextSearch';
 import ToolBar from './toolbar/ToolBar';
 import CloseButton from './toolbar/CloseButton';
 import './App.css';
 
-const isLauncherHorizontal = edge => edge === ScreenEdge.TOP;
-
 export default () => {
+  const [isCloudMode, setCloudMode] = useState(false);
+  const [isDocked, setDocked] = useState(true);
+  const [isHorizontal, setHorizontal] = useState(true);
+  const [options] = useOptions();
   const [edge, windowActions] = useDockWindow(
     ScreenEdge.TOP,
     OpenfinApiHelpers.getCurrentWindowSync(),
@@ -32,35 +34,60 @@ export default () => {
 
   const edgeToBeChecked = edge === ScreenEdge.NONE ? prevEdge : edge;
 
+  useEffect(() => {
+    setCloudMode(
+      options && options.customData && options.customData.cloudMode === true
+    );
+    setDocked(!isCloudMode && edge !== ScreenEdge.NONE);
+    setHorizontal(
+      isCloudMode || [ScreenEdge.TOP, ScreenEdge.NONE].indexOf(edge) > -1
+    );
+  }, [edge, isCloudMode, isDocked, isHorizontal, options]);
+
+  const undock = useCallback(() => {
+    const w = OpenfinApiHelpers.getCurrentWindowSync();
+    console.log(window);
+    window.resizeTo(250, 250);
+    //OpenfinApiHelpers.resizeWindow(100);
+    //console.log(w);
+  }, []);
+
   return (
     <div className={cx('app', edgeToBeChecked)}>
-      {!isLauncherHorizontal(edgeToBeChecked) && CloseButton}
-      <AppShortcuts />
       <FreeTextSearch dockedTo={edge} />
       <ToolBar
         tools={[
           {
             label: <FaChevronUp />,
             onClick: windowActions.dockTop,
-            disabled: edge === ScreenEdge.TOP
+            disabled: edge === ScreenEdge.TOP,
+            visible: !isCloudMode
           },
           {
             label: <FaChevronLeft />,
             onClick: windowActions.dockLeft,
-            disabled: edge === ScreenEdge.LEFT
+            disabled: edge === ScreenEdge.LEFT,
+            visible: !isCloudMode
           },
           {
             label: <FaChevronRight />,
             onClick: windowActions.dockRight,
-            disabled: edge === ScreenEdge.RIGHT
+            disabled: edge === ScreenEdge.RIGHT,
+            visible: !isCloudMode
+          },
+          {
+            label: <FaUnlock />,
+            onClick: undock,
+            visible: !isCloudMode && isDocked
           },
           {
             label: <FaRegHandRock />,
-            className: 'drag-handle'
+            className: 'drag-handle',
+            visible: !isDocked
           }
         ]}
       />
-      {isLauncherHorizontal(edgeToBeChecked) && CloseButton}
+      {isHorizontal && CloseButton}
     </div>
   );
 };
