5c639e0f62221483d6c10ed62afbcdbe1fe910c5
Johnston97 <lukejohnston97@live.com>
2019-10-11 12:22:57 +0100

    Navigation chart has been created

--- packages/stockflux-chart/src/components/Chart.js
+++ packages/stockflux-chart/src/components/Chart.js
@@ -44,6 +44,7 @@ const Chart = ({ symbol, date }) => {
   }, [getOHLCData]);
 
   const showcaseContainer = useRef(null);
+  const navigationContainer = useRef(null);
 
   function makeChart(data) {
 
@@ -102,6 +103,84 @@ const Chart = ({ symbol, date }) => {
       .svgPlotArea(multi)
 
 
+    var svg = d3.select("#showcase-container").append("svg")
+      .attr("width", '100%')
+      .attr("height", '20%')
+      .style("border", "1px solid lightgray");
+
+
+    console.log("ahha", svg.attr('width'))
+
+    // var navXScale = xExtent(data)
+    // console.log(navXScale)
+    // var navYScale = yExtent(data)
+    // console.log(navYScale)
+
+    var navXScale = d3.scaleTime()
+      .domain(xExtent(data))
+      .range([50, 800])
+
+    var navYScale = d3.scaleLinear()
+      .domain(yExtent(data))
+      .range([60, 0]);
+
+    var navXAxis = d3.axisBottom(navXScale)
+
+    svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + 60 + ')')
+      .call(navXAxis);
+
+    var navData = d3.area()
+      .x(function (d) { return navXScale(d.date); })
+      .y0(100)
+      .y1(function (d) { return navYScale(d.close); });
+
+    var navLine = d3.line()
+      .x(function (d) { return navXScale(d.date); })
+      .y(function (d) { return navYScale(d.close); });
+
+
+    // svg.append('path').attr('class', 'data').attr('d', navData(data))
+    svg.append('path')
+      .attr('class', 'line')
+      .attr('d', navLine(data));
+
+    // var navChart = d3.select('#showcase-container').classed('chart', true).append('svg').classed('navigator', true).append('g')
+    // navChart.append("rect")
+    //   .attr("x", 0)
+    //   .attr("y", 0)
+    //   .attr("width", '100%')
+    //   .attr("height", 100)
+    //   .style("fill", "#F5F5F5")
+    //   .style("shape-rendering", "crispEdges")
+
+    // var navData = d3.area()
+    //   .x(function (d) { return xExtent(d); })
+    //   .y1(function (d) { return yExtent(d); });
+
+    // var navLine = d3.line()
+    //   .x(function (d) { return xExtent(d); })
+    //   .y(function (d) { return yExtent(d); });
+
+    // console.log(navData)
+    // console.log("here", navData(data))
+
+    // console.log("hre", xExtent(data))
+    // console.log("hre", yExtent(data))
+    // navChart.append('path')
+    //   .attr('class', 'data')
+    //   .attr('d', navData(data));
+
+    // navChart.append('path')
+    //   .attr('class', 'line')
+    //   .attr('d', navLine(data));
+    // navChart.append('g').attr('class', 'x-axis').call(navXAxis)
+
+
+
+
+
     const render = () => {
       d3.select('#showcase-container')
         .datum(data)
@@ -123,7 +202,8 @@ const Chart = ({ symbol, date }) => {
   }
 
 
-  return <div ref={showcaseContainer} id="showcase-container" />;
+  return <div id="chart-container"><div ref={showcaseContainer} id="showcase-container" />
+    <div ref={navigationContainer} id="navigation-container" /></div>;
 };
 
 export default Chart;
