f8e64f33426d91940adc5b7460192a132625cbea
kangelSL <51117162+kangelSL@users.noreply.github.com>
2019-10-29 14:29:36 +0000

    Initial title bar changes

--- packages/stockflux-launcher/src/titlebar/Titlebar.js
+++ packages/stockflux-launcher/src/titlebar/Titlebar.js
@@ -16,45 +16,71 @@ export default ({ dockedTo }) => {
     currentWindow.close();
   }, []);
 
+  const dockedHorizontal = dockedTo === 'top' || dockedTo === 'none';
+
   let heading;
-  if (dockedTo === 'top' || dockedTo === 'none') {
+  if (dockedHorizontal) {
     heading = (
       <p className="title">
         <strong>Stock</strong>Flux
       </p>
     );
   } else {
-    heading = (
-      <p className="title">
-        <strong>SF</strong>
-      </p>
-    );
+    // heading = (
+    //   <p className="title">
+    //     <strong>SF</strong>
+    //   </p>
+    // );
   }
 
   return (
     <div className="titlebar">
-      <div
-        className={cx(
-          'column column-left',
-          dockedTo === 'top' || dockedTo === 'none' ? 'add-padding-left' : ''
-        )}
-      >
-        {heading}
-      </div>
+      {dockedHorizontal && (
+        <div
+          className={cx(
+            'column column-left',
+            dockedTo === 'top' || dockedTo === 'none' ? 'add-padding-left' : ''
+          )}
+        >
+          <p className="title">
+            <strong>Stock</strong>Flux
+          </p>
+        </div>
+      )}
+
       <div
         className={cx(
           'column column-right',
-          dockedTo === 'top' || dockedTo === 'none' ? 'add-padding-right' : ''
+          dockedTo === 'top' || dockedTo === 'none'
+            ? 'header-icon add-padding-right'
+            : 'header-icon'
         )}
+        onClick={onCloseClick}
+        title="Close"
       >
-        <div className="header-icon" onClick={onMinimizeClick} title="Minimize">
-          <MinimizeIcon />
-        </div>
-
-        <div className="header-icon" onClick={onCloseClick} title="Close">
-          <CloseIcon />
-        </div>
+        <CloseIcon />
       </div>
+
+      <div
+        className={cx(
+          'column column-right',
+          dockedTo === 'top' || dockedTo === 'none'
+            ? 'header-icon add-padding-right'
+            : 'header-icon'
+        )}
+        onClick={onMinimizeClick}
+        title="Minimize"
+      >
+        <MinimizeIcon />
+      </div>
+
+      {!dockedHorizontal && (
+        <div>
+          <p className="title title-vertical">
+            <strong>SF</strong>
+          </p>
+        </div>
+      )}
     </div>
   );
 };
