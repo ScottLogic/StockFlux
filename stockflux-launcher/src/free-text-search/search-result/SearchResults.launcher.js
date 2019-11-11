b282c55534cd24365726854e8c7c19f1990d22cf
Johnston97 <lukejohnston97@live.com>
2019-11-11 13:02:42 +0000

    fixed linting

--- stockflux-launcher/src/free-text-search/search-result/SearchResults.launcher.js
+++ stockflux-launcher/src/free-text-search/search-result/SearchResults.launcher.js
@@ -1,31 +1,47 @@
 import getWindowPosition from './SearchResults.positioner';
 import { ScreenEdge } from 'openfin-react-hooks';
 
-export default async (searchButtonRef, searchInputRef, dockedTo, windowBounds) => {
-    const { defaultTop, defaultLeft, defaultWidth, defaultHeight } = getWindowPosition(
-        searchButtonRef, searchInputRef, dockedTo, windowBounds);
+export default async (
+  searchButtonRef,
+  searchInputRef,
+  dockedTo,
+  windowBounds
+) => {
+  const {
+    defaultTop,
+    defaultLeft,
+    defaultWidth,
+    defaultHeight
+  } = getWindowPosition(
+    searchButtonRef,
+    searchInputRef,
+    dockedTo,
+    windowBounds
+  );
 
-    const childWindow = {
-        name: 'child-window-search-results',
-        defaultWidth,
-        defaultHeight,
-        url: 'searchResultsWindow.html',
-        frame: false,
-        autoShow: true,
-        defaultTop,
-        defaultLeft,
-        saveWindowState: false,
-        showTaskbarIcon: false,
-        backgroundColor: '#28313D',
-        waitForPageLoad: true,
-        alwaysOnTop: true
-    };
+  const childWindow = {
+    name: 'child-window-search-results',
+    defaultWidth,
+    defaultHeight,
+    url: 'searchResultsWindow.html',
+    frame: false,
+    autoShow: true,
+    defaultTop,
+    defaultLeft,
+    saveWindowState: false,
+    showTaskbarIcon: false,
+    backgroundColor: '#28313D',
+    waitForPageLoad: true,
+    alwaysOnTop: true
+  };
 
-    const win = await window.fin.Window.create(childWindow);
+  const win = await window.fin.Window.create(childWindow);
 
-    if (dockedTo === ScreenEdge.LEFT || dockedTo === ScreenEdge.RIGHT) {
-        win.getWebWindow().document.getElementById('searchbar-container').hidden = false;
-    }
+  if (dockedTo === ScreenEdge.LEFT || dockedTo === ScreenEdge.RIGHT) {
+    win
+      .getWebWindow()
+      .document.getElementById('searchbar-container').hidden = false;
+  }
 
-    return win;
+  return win;
 };
