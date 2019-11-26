fd7971bb8d83d08163341670b9827465c8401e7f
Johnston97 <lukejohnston97@live.com>
2019-11-26 16:03:20 +0000

    Updated shortcuts to provide correct size image

--- packages/stockflux-components/src/components/buttons/borderless-buttons/app-shortcuts/Chart.js
+++ packages/stockflux-components/src/components/buttons/borderless-buttons/app-shortcuts/Chart.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Launchers } from 'stockflux-core';
-import ChartIcon from '../../../glyphs/small/chart.svg';
-import BorderlessButton from '../../borderless-button/BorderlessButton';
+import ChartIconSmall from '../../../glyphs/small/chart.svg';
+import ChartIcon from '../../../glyphs/launcher/chart.svg';
+import RoundButton from '../../round-button/RoundButton';
 
-export default ({ symbol, name, small, disabled, onClick, intentsEnabled }) => (
-  <BorderlessButton
-    className="shortcut chart"
-    onClick={async () => {
-      await Launchers.launchChart(symbol, name, intentsEnabled);
-      if (onClick) onClick();
-    }}
-    disabled={disabled}
-    small={small}
-  >
-    <ChartIcon />
-  </BorderlessButton>
-);
+export default ({ symbol, name, small, disabled, onClick, intentsEnabled }) => {
+  if (small) {
+    return <RoundButton
+      className="shortcut chart"
+      onClick={() => Launchers.launchChart(symbol, name, intentsEnabled)}
+      disabled={disabled}
+      small={small}
+    >
+      <ChartIconSmall />
+    </RoundButton>
+  } else return (
+    <RoundButton
+      className="shortcut chart"
+      onClick={async () => {
+        await Launchers.launchChart(symbol, name, intentsEnabled);
+        if (onClick) onClick();
+      }}
+      disabled={disabled}
+      small={small}
+    >
+      <ChartIcon />
+    </RoundButton>
+  )
+};
