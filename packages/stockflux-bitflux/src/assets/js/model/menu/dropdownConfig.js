6f9e319bcd7a35c2b7c23e187a9178dfaaf9f908
Johnston97 <lukejohnston97@live.com>
2019-11-29 16:59:11 +0000

    Removed bitflux

--- packages/stockflux-bitflux/src/assets/js/model/menu/dropdownConfig.js
+++
@@ -1,8 +0,0 @@
-export default function(title, careted, listIcons, icon) {
-  return {
-    title: title || null,
-    careted: careted || false,
-    listIcons: listIcons || false,
-    icon: icon || false
-  };
-}
